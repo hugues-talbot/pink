043f6e9046997ee799568ca5176d7afdb9912fd6
Laurent Najman <laurent.najman@esiee.fr>
2009-09-17 08:42:14 +0000

    Scripts constrained connectivity

---
+++ scripts/constrainedconnectivity.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#Computation of constrain connectivity using hierarchical edge-segmentation
+USAGE="Usage: $0 ImageIn ImageOut"
+if [ $# -gt 2 ]
+then
+	echo $USAGE
+        exit
+fi
+if [ $# -lt 2 ]
+then
+	echo $USAGE
+        exit
+fi
+#Double the image size
+${PINK}/linux/bin/zoomint.exe $1 2 _double.pgm
+#Compute the gradient on the edges
+${PINK}/linux/bin/pgm2GA _double.pgm 0 _GA.pgm
+#Compute the alpha hierarchy
+${PINK}/linux/bin/saliencyGA.exe _GA.pgm 1 _alphaGA.pgm
+#Compute the alpha-omega hierarchy
+${PINK}/linux/bin/saliencyGA.exe _alphaGA.pgm 3 _alphaOmegaGA.pgm _double.pgm
+#To visualize, make it khalimsky
+${PINK}/linux/bin/GA2khalimsky.exe _alphaOmegaGA.pgm 0 $2
