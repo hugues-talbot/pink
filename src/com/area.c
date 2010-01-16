fb152cdc9b413b219844e1438e540ba2da7d7130
Michel Couprie <michel.couprie@esiee.fr>
2010-01-16 16:34:28 +0000

    update area

--- src/com/area.c
+++ src/com/area.c
@@ -36,11 +36,13 @@ knowledge of the CeCILL license and that you accept its terms.
 
 \brief return the number of non-null pixels of an image
 
-<B>Usage:</B> area in.pgm out.list
+<B>Usage:</B> area in.pgm [out.list]
 
 <B>Description:</B>
 This function returns (in the list <B>out.list</B>) 
 the number of non-null pixels of the image \b in.pgm .
+
+If the parameter \b out.list is ommitted, the result is printed on the standard output.
 
 <B>Types supported:</B> byte 2d, byte 3d, int32_t 2d, int32_t 3d, float 2d, float 3d
 
@@ -68,7 +70,7 @@ int main(int argc, char **argv)
   int32_t area;
   FILE *fd = NULL;
 
-  if (argc != 3)
+  if ((argc != 3) && (argc != 2))
   {
     fprintf(stderr, "usage: %s in.pgm out.list\n", argv[0]);
     exit(1);
@@ -87,15 +89,19 @@ int main(int argc, char **argv)
     exit(1);
   }  
 
-  fd = fopen(argv[argc - 1],"w");
-  if (!fd)
+  if (argc == 3)
   {
-    fprintf(stderr, "%s: cannot open file: %s\n", argv[0], argv[argc - 1]);
-    exit(1);
+    fd = fopen(argv[argc - 1],"w");
+    if (!fd)
+    {
+      fprintf(stderr, "%s: cannot open file: %s\n", argv[0], argv[argc - 1]);
+      exit(1);
+    }
+    fprintf(fd, "e %d\n", 1); 
+    fprintf(fd, "%d\n", area); 
+    fclose(fd);
   }
-  fprintf(fd, "e %d\n", 1); 
-  fprintf(fd, "%d\n", area); 
-  fclose(fd);
+  else printf("%d\n", area); 
 
   freeimage(image);
 
