ac5481eef38eb760332b1c5780fb22b996575519
Laurent Najman <laurent.najman@esiee.fr>
2010-10-15 15:34:47 +0000

    .exe enleve

--- scripts/constrainedconnectivity.sh
+++ scripts/constrainedconnectivity.sh
@@ -12,12 +12,12 @@ then
         exit
 fi
 #Double the image size
-${PINK}/linux/bin/zoomint.exe $1 2 _double.pgm
+${PINK}/linux/bin/zoomint $1 2 _double.pgm
 #Compute the gradient on the edges
 ${PINK}/linux/bin/pgm2GA _double.pgm 0 _GA.pgm
 #Compute the alpha hierarchy
-${PINK}/linux/bin/saliencyGA.exe _GA.pgm 1 _alphaGA.pgm
+${PINK}/linux/bin/saliencyGA _GA.pgm 1 _alphaGA.pgm
 #Compute the alpha-omega hierarchy
-${PINK}/linux/bin/saliencyGA.exe _alphaGA.pgm 3 _alphaOmegaGA.pgm _double.pgm
+${PINK}/linux/bin/saliencyGA _alphaGA.pgm 3 _alphaOmegaGA.pgm _double.pgm
 #To visualize, make it khalimsky
-${PINK}/linux/bin/GA2khalimsky.exe _alphaOmegaGA.pgm 0 $2
+${PINK}/linux/bin/GA2khalimsky _alphaOmegaGA.pgm 0 $2
