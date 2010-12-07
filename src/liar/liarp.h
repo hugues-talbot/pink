4063bca82fba111f4b6fb31edee84fb559722d30
Hugues Talbot <hugues.talbot@gmail.com>
2010-12-07 02:01:18 +0000

    stuff from Australia

---
+++ src/liar/liarp.h
@@ -0,0 +1,12 @@
+/*------------------------------------------------------------------------
+ *
+ * Prototypes for the minimal version of the
+ * Library of Image Analysis Routines
+ *
+ * From Imview
+ *
+ * Hugues Talbot	 4 Jan 2001
+ *      
+ *-----------------------------------------------------------------------*/
+
+int LIARdebug(char *fmt, ...);
