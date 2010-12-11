6221c91ed37b937747d7f182e4dcb558d974cf80
Laszlo Marak <ujoimro@gmail.com>
2010-12-11 17:52:15 +0000

    visualisation works I quickly save before I change something and it disappears

--- python/pink/render.py
+++ python/pink/render.py
@@ -8,149 +8,143 @@ import Tkinter as tk
 import tkSimpleDialog
 from pink.cpp import mcube
 
-
-def bye():
-    root.quit()
-
-
-
-def savepng():        
-    global root
-    w2i=vtk.vtkWindowToImageFilter()
-    writer = vtk.vtkPNGWriter()
-    w2i.SetInput(ren_win)
-    writer.SetInput(w2i.GetOutput())
-    writer.SetFileName(str(tkSimpleDialog.askstring( "Save a Snapshot", "Specify the filename to save a snapshot:", initialvalue="_sav01.png", parent=root)))
-    writer.Write()
-
-
-def togglewire1():
-    if int(wireframe1.get())==1:
-        objActor1.GetProperty().SetRepresentationToWireframe()
-    else:
-        objActor1.GetProperty().SetRepresentationToSurface()        
-    ren_win.Render()
-
-def togglewire2():
-    if int(wireframe2.get())==1:
-        objActor2.GetProperty().SetRepresentationToWireframe()
-    else:
-        objActor2.GetProperty().SetRepresentationToSurface()        
-    ren_win.Render()
-
-
-def set_opacity(f):
-    global objActor1
-    global ren_win
-
-    objActor1.GetProperty().SetOpacity(float(f))
-    ren_win.Render()
-
-
-
-root = tk.Toplevel()
-ren1 = vtk.vtkRenderer()
-ren_win = vtk.vtkRenderWindow()
-ren_win.AddRenderer(ren1)
-
-pdr1 = vtk.vtkPolyDataReader()
-pdr2 = vtk.vtkPolyDataReader()
-normals1 = vtk.vtkPolyDataNormals()
-normals2 = vtk.vtkPolyDataNormals()
-objMapper1 = vtk.vtkPolyDataMapper()
-objMapper2 = vtk.vtkPolyDataMapper()
-objActor1 = vtk.vtkActor()
-objActor2 = vtk.vtkActor()
-ren1.AddViewProp(objActor1)
-ren1.AddViewProp(objActor2)
-vtkw = vtkTkRenderWidget(root, width=600, height=600, rw=ren_win)
-
-# # then binds rendering window to a tk widget        
-root.title("Multiple Renderer")
-
-vtkw.BindTkRenderWidget()
-
-commands = tk.Frame(root)
-buttons = tk.Frame(root)
-
-buttons.pack( fill="both", expand=1, side="top" )
-commands.pack( fill="both", expand=1, side="bottom" )
-quitbutton = tk.Button(buttons, text="Exit", activebackground="white", command=bye)
-savebutton = tk.Button(buttons, text="Save", activebackground="white", command=savepng)        
-
-wireframe1 = tk.IntVar()
-wireframe1.set(0)
-button = tk.Checkbutton(buttons, text="Wireframe1", variable=wireframe1, activebackground="white", command=togglewire1)
-button.pack(side="left")
-
-wireframe2 = tk.IntVar()
-wireframe2.set(0)
-button = tk.Checkbutton(buttons, text="Wireframe2", variable=wireframe2, activebackground="white", command=togglewire2)
-button.pack(side="left")
-
-
-
-
-opacity = tk.Scale(commands, from_=0.0, to=1.0, res=0.01, orient="horizontal", label="Opacity", command=set_opacity)
-
-quitbutton.pack(side="right")
-savebutton.pack(side="left")        
-vtkw.pack(side="top", fill="both", expand="yes")
-opacity.pack(side="top", anchor="nw", fill="both")
-
-    
-
-
-
-
-
-
-
-
-
-def render_images(mesh1, mesh2=0):
-    global ren1        
-    global ren_win
-    
-    ren1.SetViewport([0, 0, 1, 1])
-    ren1.SetBackground([0.93, 0.93, 0.97])
-    
-    pdr1.SetInputString(mesh1)
-    pdr1.ReadFromInputStringOn()
-    normals1.SetInput(pdr1.GetOutput())
-    normals1.FlipNormalsOn()
-    objMapper1.SetInput(normals1.GetOutput())
-    objActor1.SetMapper(objMapper1)
-
-
-    # optional second object
-    if mesh2!=0:        
-        objActor2.GetProperty().SetColor([1.0, 0.49, 0.25])
-        pdr2.SetInputString(mesh2)
-        pdr2.ReadFromInputStringOn()
-        normals2.SetInput(pdr2.GetOutput())
-        normals2.FlipNormalsOn()
-        objMapper2.SetInput(normals2.GetOutput())
-        objActor2.SetMapper(objMapper2)
-
+class render_class:
+    def __init__(self, mesh1, mesh2=0):
+        self.name = "render"
+        self.rs = 600
+        self.cs = 600
+        self.mesh1 = mesh1
+        self.mesh2 = mesh2
+        self.ren1 = vtk.vtkRenderer()
+        self.ren1.SetViewport([0, 0, 1, 1])
+        self.ren1.SetBackground([0.93, 0.93, 0.97])
+        self.ren_win = vtk.vtkRenderWindow()
+        self.ren_win.AddRenderer(self.ren1)
         
-        
-    ren1.ResetCamera()
-    ren_win.Render()
+        # load geometry
+        ## first object
+        self.obj1 = obj(mesh1)        
+        self.ren1.AddViewProp(self.obj1.objActor)
+
+        ## optional second object
+        if mesh2!=0:
+            self.obj2 = obj(mesh2)
+            self.obj2.objActor.GetProperty().SetColor([1.0, 0.49, 0.25])
+            self.ren1.AddViewProp(self.obj2.objActor)
+
+        self.ren1.ResetCamera()
+
+
+class obj:
+    def __init__(self, mesh):
+        self.obj = vtk.vtkPolyDataReader()
+        self.obj.SetInputString(mesh)
+        self.obj.ReadFromInputStringOn()
+        self.normals = vtk.vtkPolyDataNormals()
+        self.normals.SetInput(self.obj.GetOutput())
+        self.normals.FlipNormalsOn()
+        self.objMapper = vtk.vtkPolyDataMapper()
+        self.objMapper.SetInput(self.normals.GetOutput())
+        self.objActor = vtk.vtkActor()
+        self.objActor.SetMapper(self.objMapper)
 
     
-def render(image1, image2=0):
-    global root 
-    #global application
+class app:
+    def __init__(self, render):
+        self.render = render # the vtk renderer object
+
+        # # then binds rendering window to a tk widget
+        self.root = tk.Toplevel()
+        self.root.title("Multiple Renderer")
+        self.vtkw = vtkTkRenderWidget(self.root, width=600, height=600, rw=self.render.ren_win)
+        self.vtkw.BindTkRenderWidget()
+
+        self.commands = tk.Frame(self.root)
+        self.buttons = tk.Frame(self.root)
+
+        self.buttons.pack( fill="both", expand=1, side="top" )
+        self.commands.pack( fill="both", expand=1, side="bottom" )
+        self.quitbutton = tk.Button(self.buttons, text="Exit", activebackground="white", command=self.bye)
+        self.savebutton = tk.Button(self.buttons, text="Save", activebackground="white", command=self.savepng)        
+        self.wirebutton1 = wirebutton(frame=self.buttons, text="Wireframe1", ren_win=self.render.ren_win, obj_actor=self.render.obj1.objActor)
+
+        if render.mesh2!=0:
+            self.wirebutton1 = wirebutton(frame=self.buttons, text="Wireframe2", ren_win=self.render.ren_win, obj_actor=self.render.obj2.objActor)
+
+        self.opacity = tk.Scale(self.commands, from_=0.0, to=1.0, res=0.01, orient="horizontal", label="Opacity", command=self.set_opacity)
+                
+        self.quitbutton.pack(side="right")
+        self.savebutton.pack(side="left")        
+        self.vtkw.pack(side="top", fill="both", expand="yes")
+        self.opacity.pack(side="top", anchor="nw", fill="both")
+        
+
+    def set_opacity(self, f):
+        self.render.obj1.objActor.GetProperty().SetOpacity(float(f))
+        self.render.ren_win.Render()
+
+    def mainloop(self):
+        self.root.mainloop()
+    
+    def bye(self):
+    #vtkCommand DeleteAllObjects
+        self.root.quit()
+
+
+    def savepng(self):
+        
+        w2i=vtk.vtkWindowToImageFilter()
+        writer = vtk.vtkPNGWriter()
+        w2i.SetInput(self.render.ren_win)
+        writer.SetInput(w2i.GetOutput())
+        writer.SetFileName(str(tkSimpleDialog.askstring( "Save a Snapshot", "Specify the filename to save a snapshot:", initialvalue="_sav01.png", parent=self.root)))
+        writer.Write()
+        
+
+
+
+class wirebutton:
+    def __init__(self, frame, text, ren_win, obj_actor):
+        self.ren_win = ren_win
+        self.obj_actor = obj_actor
+        self.wireframe = tk.IntVar()
+        self.wireframe.set(0)
+        self.button = tk.Checkbutton(frame, text=text, variable=self.wireframe, activebackground="white", command=self.togglewire)
+        self.button.pack(side="left")
+
+    def togglewire(self):
+        if int(self.wireframe.get())==1:
+            self.obj_actor.GetProperty().SetRepresentationToWireframe()
+        else:
+            self.obj_actor.GetProperty().SetRepresentationToSurface()
+            
+        self.ren_win.Render()
+
+
+
+# def renderfiles( filename1, filename2="" ):
+#     render_obj = render_class( filename1, filename2 )
+#     #renderer( "/home/ujoimro/doc/projects/Tutorial-Pink/MC-TP3/solution/TP3cor/lettre_a.vtk",
+#     #                     "/home/ujoimro/doc/projects/Tutorial-Pink/MC-TP3/solution/TP3cor/carotide_seg.vtk"
+#     #                     )
+#     application = app(render_obj)
+#     application.mainloop()
+
+def render( image1, image2=0 ):
     mesh1 = mcube(image1, 0, 0, 0)
     if image2!=0:
         mesh2 = mcube(image2, 0, 0, 0)
     else:
         mesh2 = 0
 
-    render_images(mesh1, mesh2)
-    print "Rendering"
-    root.mainloop()
+    render_obj=render_class(mesh1, mesh2)
+    application = app(render_obj)
+    application.mainloop()
+    #mesh2 = 
+ 
+
+
+
 
 
 
