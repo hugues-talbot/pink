a3e25c110fa389dce8da157848721e105918c544
Laszlo Marak <ujoimro@gmail.com>
2011-06-29 14:24:06 +0000

    towards conforming with msvc

--- src/liar/liarwrap.c
+++ src/liar/liarwrap.c
@@ -116,8 +116,13 @@ int LIARdebug(const char * msg,...)
 
     if (debugIsOn) {
         im_va_start(args,msg);
+
+#       ifdef WIN32
+	_snprintf(strarg, BUFSIZE, DEBUGPROMPT "%s\n", msg);
+#       else /* NOT WIN32 */
 	snprintf(strarg, BUFSIZE, DEBUGPROMPT "%s\n", msg);
-        
+#       endif /* NOT WIN32 */
+
         //ret = vfprintf(stderr,strarg,args);
         ret = vprintf(strarg, args);
         va_end(args);
@@ -166,7 +171,12 @@ int imexception(const char *msg, ...)
 
     im_va_start(args,msg);
 
+#   ifdef WIN32
+    _snprintf(strarg, BUFSIZE, EXCEPTPROMPT "%s", msg);
+#   else /* NOT WIN32 */
     snprintf(strarg, BUFSIZE, EXCEPTPROMPT "%s", msg);
+#   endif /* NOT WIN32 */
+
     ret = vfprintf(stderr, strarg, args);
 
     va_end(args);
