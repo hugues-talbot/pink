7113957b115de5fa6512375b136a9fcd6713d6c5
Laszlo Marak <ujoimro@gmail.com>
2011-06-29 23:55:46 +0000

    setup.py for creating a windows installer. Visual Studio runtime redistributable package also needed

---
+++ setup.py
@@ -0,0 +1,23 @@
+# UjoImro, 2011
+# Pink's Python installer
+# This file is used to generate a Python installer
+# executable on Microsoft Windows
+
+
+from distutils.core import setup
+
+setup( name         = 'pink-image',
+       version      = '0.9.r',
+       description  = 'Pink Image Processing Library',
+       author       = 'Michel Couprie et al.',
+       author_email = 'pink-image-devel@lists.berlios.de',
+       license      = 'CeCILL',
+       url          = 'http://www.pinkhq.com',
+       packages     = [ 'pink', 'pink.MICCAI' ],
+       data_files = [ ('DLLs', ['libcpp_pink.pyd'])]
+     )
+
+
+
+
+# LuM end of file
