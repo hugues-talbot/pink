e0d0ff693dd33779af86952a3ac9291732238e55
Hugues Talbot <hugues.talbot@gmail.com>
2012-01-12 19:02:03 +0000

    better solution

--- tutorial/python/HT-TP1/solution/ht-tp1.py
+++ tutorial/python/HT-TP1/solution/ht-tp1.py
@@ -9,24 +9,31 @@ from pink import cpp as pink
 #ex 3.1-1
 #help(pink.geoeros)
 
-viewer1=Imview()
-
 #ex 3.1-2
 I=pink.readimage("../images/cells.pgm")
+
+viewer1=Imview(I)
+
 eroded = pink.erosball( I, 1, 0 )
-viewer1.show(eroded)
+viewer1.show(eroded,"eroded") # spacebar to switch from one image to the next
 
 #ex 3.1-3
-recons1 = pink.geodilat(eroded, I, 8, 10)  
+recons1 = pink.geodilat(eroded, I, 8, 10)
+viewer1.show(recons1,"recons1")
 #recons1.writeimage("recons1.pgm")
 
 
 #ex 3.1-4
-dilated = pink.dilatball(I, 1, 0)
-recons2 = pink.geoeros(dilated, I, 8, 10)
+# we start from the already partially denoised result
+dilated = pink.dilatball(recons1, 1, 0)
+recons2 = pink.geoeros(dilated, recons1, 8, 10)
+
+# we save this result
 #recons2.writeimage("recons2.pgm")
 
 #ex 3.1-5
+
+# a procedure to do everything as a function
 def filter_noise(image):
     eroded  = pink.erosball( image, 1, 0 )
     recons1 = pink.geodilat(eroded, image, 8, 10)
@@ -35,32 +42,36 @@ def filter_noise(image):
     return result
 
 cells_filt = filter_noise(I)
-#cells_filt.writeimage("cells_filt.pgm")
+# save the result
+cells_filt.writeimage("cells_filt.pgm")
 
-
+###
 #ex 3.2-2
 def remove_touching( image ):
     frame  = pink.frame(image, 0)
     seeds  = image - frame
     #seeds.writeimage("seeds.pgm")
-    result = image - pink.geodilat(seeds, image, 8, 100)
+    result = image - pink.geodilat(seeds, image, 8, -1)
     return result
     
-cells_nohole = remove_touching(cells_filt)
+cells_notouch = remove_touching(cells_filt)
+viewer1.show(cells_notouch,"cells_notouch")
 #cells_nohole.writeimage("cells_nohole.pgm")
 
 
 #ex 3.3-2
-inv = pink.inverse(cells_nohole)
+inv = pink.inverse(cells_notouch)
 holes = remove_touching(inv)
 
 cells_filled = pink.max(cells_nohole, holes)
+viewer1.show(cells_filled,"cells_filled")
 #cells_filled.writeimage("cells_filled.pgm")
 
 
 #ex 3.4-2
-objects = pink.geodilat( holes, cells_filled, 8, 100 )
+objects = pink.geodilat( holes, cells_filled, 8, -1 )
 cells_final = pink.min( cells_nohole, objects )
+viewer1.show(cells_final,"cells_final")
 #cells_final.writeimage("cells_final.pgm")
 
 #ex 4
@@ -69,6 +80,7 @@ circuit = pink.readimage("../images/circuit.pgm")
 inv = pink.inverse(circuit)
 inv = pink.seuil(inv, 180, 0 ,255)
 inv = filter_noise(inv)
+viewer2=Imview(inv)
 #inv.writeimage("circ.pgm")
 
 ## dilation
