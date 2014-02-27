8e19d9cbbfec0491d876b7a40b555682838d330c
Laszlo Marak <ujoimro@gmail.com>
2014-02-27 13:53:52 +0100

    include/ui_decompose.hpp selected for remove

--- include/ui_decompose.hpp
+++ include/ui_decompose.hpp
@@ -24,73 +24,73 @@
 // ...
 // decompose<Fn>::TN           - the types of the function's parameters
 
-#ifndef MAX_PARAMETERS
-#  define MAX_PARAMETERS 15
-#  define CLEAN_UP_MAX_PARAMETERS
-#endif /* MAX_PARAMETERS */
+// #ifndef MAX_PARAMETERS
+// #  define MAX_PARAMETERS 15
+// #  define CLEAN_UP_MAX_PARAMETERS
+// #endif /* MAX_PARAMETERS */
 
-template <class Fn> struct decompose;
+// template <class Fn> struct decompose;
 
-#define PARAMETERS(z, n, text)                  \
-  typedef PT##n T##n;                           
+// #define PARAMETERS(z, n, text)                  \
+//   typedef PT##n T##n;                           
   
 
-#define DECOMPOSE(z, n, text)                   \
-  template< class R BOOST_PP_COMMA_IF(n) BOOST_PP_ENUM_PARAMS(n, class PT) > \
-  struct decompose <R(*)(BOOST_PP_ENUM_PARAMS(n, PT))>          \
-  {                                                             \
-    static const unsigned arity = n;                            \
-    typedef R result_type;                                      \
-    BOOST_PP_REPEAT(n, PARAMETERS, ~);                          \
-  };
+// #define DECOMPOSE(z, n, text)                   \
+//   template< class R BOOST_PP_COMMA_IF(n) BOOST_PP_ENUM_PARAMS(n, class PT) > \
+//   struct decompose <R(*)(BOOST_PP_ENUM_PARAMS(n, PT))>          \
+//   {                                                             \
+//     static const unsigned arity = n;                            \
+//     typedef R result_type;                                      \
+//     BOOST_PP_REPEAT(n, PARAMETERS, ~);                          \
+//   };
 
 
 
 
-BOOST_PP_REPEAT(MAX_PARAMETERS, DECOMPOSE, ~);
+// BOOST_PP_REPEAT(MAX_PARAMETERS, DECOMPOSE, ~);
 
 
-// cleaning up after us
-#undef PARAMETERS
-#undef DECOMPOSE
-#ifdef CLEAN_UP_MAX_PARAMETERS
-#  undef MAX_PARAMETERS
-#endif /* CLEAN_UP_MAX_PARAMETERS */
+// // cleaning up after us
+// #undef PARAMETERS
+// #undef DECOMPOSE
+// #ifdef CLEAN_UP_MAX_PARAMETERS
+// #  undef MAX_PARAMETERS
+// #endif /* CLEAN_UP_MAX_PARAMETERS */
 
 
-// the generated code is:
+// // the generated code is:
 
-// template <class Fn> struct decomposer;
-// template< class R, > struct decompose <R(*)()> {
-//   static const unsigned arity = 0;
-//   typedef R result_type; ;
-// };
-// template< class R, class PT0 > struct decompose <R(*)( PT0)> {
-//   static const unsigned arity = 1;
-//   typedef R result_type;
-//   typedef PT0 T0;;
-// };
-// template< class R, class PT0 , class PT1 > struct decompose <R(*)( PT0 , PT1)> {
-//   static const unsigned arity = 2;
-//   typedef R result_type;
-//   typedef PT0 T0;
-//   typedef PT1 T1;;
-// };
-// template< class R, class PT0 , class PT1 , class PT2 > struct decompose <R(*)( PT0 , PT1 , PT2)> {
-//   static const unsigned arity = 3;
-//   typedef R result_type;
-//   typedef PT0 T0;
-//   typedef PT1 T1;
-//   typedef PT2 T2;;
-// };
-// template< class R, class PT0 , class PT1 , class PT2 , class PT3 > struct decompose <R(*)( PT0 , PT1 , PT2 , PT3)> {
-//   static const unsigned arity = 4;
-//   typedef R result_type;
-//   typedef PT0 T0;
-//   typedef PT1 T1;
-//   typedef PT2 T2;
-//   typedef PT3 T3;;
-// };;
+// // template <class Fn> struct decomposer;
+// // template< class R, > struct decompose <R(*)()> {
+// //   static const unsigned arity = 0;
+// //   typedef R result_type; ;
+// // };
+// // template< class R, class PT0 > struct decompose <R(*)( PT0)> {
+// //   static const unsigned arity = 1;
+// //   typedef R result_type;
+// //   typedef PT0 T0;;
+// // };
+// // template< class R, class PT0 , class PT1 > struct decompose <R(*)( PT0 , PT1)> {
+// //   static const unsigned arity = 2;
+// //   typedef R result_type;
+// //   typedef PT0 T0;
+// //   typedef PT1 T1;;
+// // };
+// // template< class R, class PT0 , class PT1 , class PT2 > struct decompose <R(*)( PT0 , PT1 , PT2)> {
+// //   static const unsigned arity = 3;
+// //   typedef R result_type;
+// //   typedef PT0 T0;
+// //   typedef PT1 T1;
+// //   typedef PT2 T2;;
+// // };
+// // template< class R, class PT0 , class PT1 , class PT2 , class PT3 > struct decompose <R(*)( PT0 , PT1 , PT2 , PT3)> {
+// //   static const unsigned arity = 4;
+// //   typedef R result_type;
+// //   typedef PT0 T0;
+// //   typedef PT1 T1;
+// //   typedef PT2 T2;
+// //   typedef PT3 T3;;
+// // };;
 
 
 
