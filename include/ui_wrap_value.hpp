76a6175d31851c26ba37a3ec7c4795fe8c8d8bfb
Laszlo Marak <ujoimro@gmail.com>
2014-02-27 13:59:47 +0100

    include/ui_wrap_value.hpp selected for removal

--- include/ui_wrap_value.hpp
+++ include/ui_wrap_value.hpp
@@ -31,98 +31,98 @@
 #include "ui_make_value.hpp"
 
 
-using pink::python::xv2pink;
-using pink::python::make_value;
+// using pink::python::xv2pink;
+// using pink::python::make_value;
 
 
-using boost::mpl::if_c;
-using boost::mpl::int_;
-using boost::enable_if;
-//using boost::mpl::is_same;
-using boost::is_same;
+// using boost::mpl::if_c;
+// using boost::mpl::int_;
+// using boost::enable_if;
+// //using boost::mpl::is_same;
+// using boost::is_same;
 
 
 
-#ifndef MAX_PARAMETERS
-#  define MAX_PARAMETERS 15
-#  define CLEAN_UP_MAX_PARAMETERS
-#endif /* MAX_PARAMETERS */
+// #ifndef MAX_PARAMETERS
+// #  define MAX_PARAMETERS 15
+// #  define CLEAN_UP_MAX_PARAMETERS
+// #endif /* MAX_PARAMETERS */
 
-#define UI_WRAP_VALUE_CALL_DEF(z, n, text)      \
-  def(fname, MAKE_FUNCTION_(z, n, text), doc )
+// #define UI_WRAP_VALUE_CALL_DEF(z, n, text)      \
+//   def(fname, MAKE_FUNCTION_(z, n, text), doc )
 
-#define UI_WRAP_VALUE_DECOMPOSE_PARAMS(z, n, text)      \
-  FT::T##n
+// #define UI_WRAP_VALUE_DECOMPOSE_PARAMS(z, n, text)      \
+//   FT::T##n
 
-#define UI_WRAP_VALUE_XV2PINK(z, n, text)                       \
-  typename xv2pink< typename BOOST_PP_CAT(text, n) >::type,
+// #define UI_WRAP_VALUE_XV2PINK(z, n, text)                       \
+//   typename xv2pink< typename BOOST_PP_CAT(text, n) >::type,
 
-#define UI_WRAP_VALUE_DEF_FOR_TYPE(image_type, n, text)                 \
-  def(fname, make_value< image_type,                                 \
-      BOOST_PP_REPEAT_FROM_TO(0, n, UI_WRAP_VALUE_XV2PINK, FT::T)       \
-      text>, args, doc)
+// #define UI_WRAP_VALUE_DEF_FOR_TYPE(image_type, n, text)                 \
+//   def(fname, make_value< image_type,                                 \
+//       BOOST_PP_REPEAT_FROM_TO(0, n, UI_WRAP_VALUE_XV2PINK, FT::T)       \
+//       text>, args, doc)
 
-#define UI_WRAP_VALUE_MAKE_INSTANCE(z, n, text)                         \
-  template <class FA>                                                   \
-  struct BOOST_PP_CAT(instance, BOOST_PP_COUNTER)< FA,                  \
-                                                   typename enable_if< is_same<FA, int_<n> > >::type > \
-  {                                                                     \
-    template <class Fn, class Args>                                     \
-      void define(const char* fname, Fn fn, Args args, const char* doc) \
-    {                                                                   \
-      typedef struct decompose<Fn> FT;                                  \
-                                                                        \
-      UI_WRAP_VALUE_DEF_FOR_TYPE(typename FT::result_type , n, text);   \
-                                                                        \
-    }                                                                   \
-                                                                        \
-  };                                                                    
-
-
-// // checking for the proper usage
-// #ifndef BOOST_PP_COUNTER
-// #error: UI_WRAP_VALUE only makes sense with BOOST_PP_COUNTER defined
-// #endif /* BOOST_PP_COUNTER */
-
-#define UI_WRAP_VALUE(FNAME, FN, ARGS, DOC)                             \
-  template <class T, class enabled = void>                              \
-  struct BOOST_PP_CAT(instance, BOOST_PP_COUNTER)                       \
-  {                                                                     \
-  };                                                                    \
-                                                                        \
-  BOOST_PP_REPEAT_FROM_TO(1, MAX_PARAMETERS, UI_WRAP_VALUE_MAKE_INSTANCE, FN) \
-                                                                        \
-  template <class Fn, class Args>                                       \
-  void BOOST_PP_CAT(call,BOOST_PP_COUNTER)(const char* fname, Fn fn, Args args, const char* doc) \
-  {                                                                     \
-    const int FA = decompose<Fn>::arity;                                \
-                                                                        \
-    BOOST_PP_CAT(instance,BOOST_PP_COUNTER)< int_< FA > > wrapped_function; \
-                                                                        \
-    wrapped_function.define(fname, fn, args, doc);                      \
-                                                                        \
-  }                                                                     \
-                                                                        \
-  void BOOST_PP_CAT(export_function, BOOST_PP_COUNTER)()                \
-  {                                                                     \
-    BOOST_PP_CAT(call, BOOST_PP_COUNTER)(FNAME, FN, ARGS, DOC);         \
-  }
+// #define UI_WRAP_VALUE_MAKE_INSTANCE(z, n, text)                         \
+//   template <class FA>                                                   \
+//   struct BOOST_PP_CAT(instance, BOOST_PP_COUNTER)< FA,                  \
+//                                                    typename enable_if< is_same<FA, int_<n> > >::type > \
+//   {                                                                     \
+//     template <class Fn, class Args>                                     \
+//       void define(const char* fname, Fn fn, Args args, const char* doc) \
+//     {                                                                   \
+//       typedef struct decompose<Fn> FT;                                  \
+//                                                                         \
+//       UI_WRAP_VALUE_DEF_FOR_TYPE(typename FT::result_type , n, text);   \
+//                                                                         \
+//     }                                                                   \
+//                                                                         \
+//   };                                                                    
 
 
-// DEBUG MODE
-// the following preprocessor call would generate the below code
+// // // checking for the proper usage
+// // #ifndef BOOST_PP_COUNTER
+// // #error: UI_WRAP_VALUE only makes sense with BOOST_PP_COUNTER defined
+// // #endif /* BOOST_PP_COUNTER */
 
-// #define BOOST_PP_COUNTER 0
+// #define UI_WRAP_VALUE(FNAME, FN, ARGS, DOC)                             \
+//   template <class T, class enabled = void>                              \
+//   struct BOOST_PP_CAT(instance, BOOST_PP_COUNTER)                       \
+//   {                                                                     \
+//   };                                                                    \
+//                                                                         \
+//   BOOST_PP_REPEAT_FROM_TO(1, MAX_PARAMETERS, UI_WRAP_VALUE_MAKE_INSTANCE, FN) \
+//                                                                         \
+//   template <class Fn, class Args>                                       \
+//   void BOOST_PP_CAT(call,BOOST_PP_COUNTER)(const char* fname, Fn fn, Args args, const char* doc) \
+//   {                                                                     \
+//     const int FA = decompose<Fn>::arity;                                \
+//                                                                         \
+//     BOOST_PP_CAT(instance,BOOST_PP_COUNTER)< int_< FA > > wrapped_function; \
+//                                                                         \
+//     wrapped_function.define(fname, fn, args, doc);                      \
+//                                                                         \
+//   }                                                                     \
+//                                                                         \
+//   void BOOST_PP_CAT(export_function, BOOST_PP_COUNTER)()                \
+//   {                                                                     \
+//     BOOST_PP_CAT(call, BOOST_PP_COUNTER)(FNAME, FN, ARGS, DOC);         \
+//   }
 
-// UI_WRAP_VALUE("greet", greet, (arg("image"), (arg("int"))) ,"WRITE ME!!");
 
-// #undef BOOST_PP_COUNTER
-// #define BOOST_PP_COUNTER 1
+// // DEBUG MODE
+// // the following preprocessor call would generate the below code
 
-// UI_WRAP_VALUE("new_greet", greeter, (arg("image"), arg("int"), arg("connexity")=0) ,"READ ME!!");
+// // #define BOOST_PP_COUNTER 0
+
+// // UI_WRAP_VALUE("greet", greet, (arg("image"), (arg("int"))) ,"WRITE ME!!");
+
+// // #undef BOOST_PP_COUNTER
+// // #define BOOST_PP_COUNTER 1
+
+// // UI_WRAP_VALUE("new_greet", greeter, (arg("image"), arg("int"), arg("connexity")=0) ,"READ ME!!");
 
 
-// the generated code
+// // the generated code
 
 
 
