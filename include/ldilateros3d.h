c7f0ba09eae88d9cba212a06764092705bef7087
Laszlo Marak <ujoimro@gmail.com>
2014-03-01 22:25:24 +0100

    ldilateros3d parameters changed to const, where appropriate

--- include/ldilateros3d.h
+++ include/ldilateros3d.h
@@ -35,20 +35,20 @@ knowledge of the CeCILL license and that you accept its terms.
 #ifdef __cplusplus
 extern "C" {
 #endif
-extern int32_t ldilateros3d_ldilatbin3d(struct xvimage *f, struct xvimage *m, int32_t xc, int32_t yc, int32_t zc);
+extern int32_t ldilateros3d_ldilatbin3d(struct xvimage *f, const struct xvimage *m, int32_t xc, int32_t yc, int32_t zc);
 extern int32_t ldilatbin3d2(struct xvimage *f, int32_t nptb, int32_t *tab_es_x, int32_t *tab_es_y, int32_t *tab_es_z, 
 			int32_t xc, int32_t yc, int32_t zc);
-extern int32_t ldilateros3d_lerosbin3d(struct xvimage *f, struct xvimage *m, int32_t xc, int32_t yc, int32_t zc);
+extern int32_t ldilateros3d_lerosbin3d(struct xvimage *f, const struct xvimage *m, int32_t xc, int32_t yc, int32_t zc);
 extern int32_t ldilateros3d_lerosbin3d2(struct xvimage *f, int32_t nptb, int32_t *tab_es_x, int32_t *tab_es_y, int32_t *tab_es_z, int32_t xc, int32_t yc, int32_t zc);
-extern int32_t ldilateros3d_ldilatfast3d(struct xvimage *f, uint8_t *mask);
+extern int32_t ldilateros3d_ldilatfast3d(struct xvimage *f, const uint8_t *mask);
 
-extern int32_t ldilatlong(struct xvimage *f, struct xvimage *m, int32_t xc, int32_t yc);
+extern int32_t ldilatlong(struct xvimage *f, const struct xvimage *m, int32_t xc, int32_t yc);
 
-extern int32_t ldilateros3d_lerosfast3d(struct xvimage *f, uint8_t *mask);
+extern int32_t ldilateros3d_lerosfast3d(struct xvimage *f, const uint8_t *mask);
 extern int32_t lerosbin3d2(struct xvimage *f, int32_t nptb, int32_t *tab_es_x, int32_t *tab_es_y, int32_t *tab_es_z, 
 			int32_t xc, int32_t yc, int32_t zc);
-extern int32_t ldilat3d(struct xvimage *f, struct xvimage *m, int32_t xc, int32_t yc, int32_t zc);
-extern int32_t leros3d(struct xvimage *f, struct xvimage *m, int32_t xc, int32_t yc, int32_t zc);
+extern int32_t ldilat3d(struct xvimage *f, const struct xvimage *m, int32_t xc, int32_t yc, int32_t zc);
+extern int32_t leros3d(struct xvimage *f, const struct xvimage *m, int32_t xc, int32_t yc, int32_t zc);
 #ifdef __cplusplus
 }
 #endif
