f2bf8e2f3004a6ae1a300012c5768fecdb8d70f9
Laszlo Marak <ujoimro@gmail.com>
2014-03-01 22:23:33 +0100

    compiled function for python export

---
+++ src/python/pyexport.cpp
@@ -0,0 +1,53 @@
+/*
+  This software is licensed under 
+  CeCILL FREE SOFTWARE LICENSE AGREEMENT
+
+  This software comes in hope that it will be useful but 
+  without any warranty to the extent permitted by applicable law.
+  
+  (C) UjoImro, 2010
+  Université Paris-Est, Laboratoire d'Informatique Gaspard-Monge, Equipe A3SI, ESIEE Paris, 93162, Noisy le Grand CEDEX
+  ujoimro@gmail.com
+*/
+
+/**   \file pyexport.hpp
+
+      \ingroup development
+
+      \brief This file defines the macros, which facilitate the export
+      of functions in Python. The emphasis is given to multiple
+      inclusion of template functions with all different types. As
+      python cannot 'compile' in runtime, you must compile the
+      template functions with all the possible image types. The macros
+      and templates in this file ease you do that.
+*/
+
+// boost python wrapper
+// This macro exports a template function to all possible image types
+#ifndef PYEXPORT_HPP
+#define PYEXPORT_HPP
+
+#include "ujimage.hpp"
+
+namespace pink {
+  
+  const xvimage *
+  can_be_null( const pink::cxvimage & image )
+  {
+    if (image.isnull())
+      return NULL;        
+    else /* NOT image.isnull() */
+      return image;        
+  } /* can_be_null */
+  
+} // namespace pink
+
+
+
+
+
+
+
+
+#endif /* PYEXPORT_HPP */
+// LuM end of file
