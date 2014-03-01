db605b96736ec8c46e3bdaa7a524b97ab4a6c106
Laszlo Marak <ujoimro@gmail.com>
2014-03-01 22:24:18 +0100

    ldilateros parameters changed to const, where appropriate

--- include/ldilateros.h
+++ include/ldilateros.h
@@ -40,20 +40,20 @@ knowledge of the CeCILL license and that you accept its terms.
 #ifdef __cplusplus
 extern "C" {
 #endif
-extern int32_t ldilatnum( struct xvimage *f, struct xvimage *m, int32_t xc, int32_t yc);
-extern int32_t lerosnum( struct xvimage *f, struct xvimage *m, int32_t xc, int32_t yc);
-extern int32_t ldilateros_ldilatbin(struct xvimage *f, struct xvimage *m, int32_t xc, int32_t yc);
-extern int32_t ldilateros_lerosbin(struct xvimage *f, struct xvimage *m, int32_t xc, int32_t yc);
-extern int32_t ldilateros_ldilatfast(struct xvimage *f, uint8_t *mask) ;
-extern int32_t ldilateros_lerosfast(struct xvimage *f, uint8_t *mask);
-extern int32_t ldilateros_ldilat(struct xvimage *f, struct xvimage *m, int32_t xc, int32_t yc);
-extern int32_t ldilateros_leros(struct xvimage *f, struct xvimage *m, int32_t xc, int32_t yc);
+extern int32_t ldilatnum( struct xvimage *f, const struct xvimage *m, int32_t xc, int32_t yc);
+extern int32_t lerosnum( struct xvimage *f, const struct xvimage *m, int32_t xc, int32_t yc);
+extern int32_t ldilateros_ldilatbin(struct xvimage *f, const struct xvimage *m, int32_t xc, int32_t yc);
+extern int32_t ldilateros_lerosbin(struct xvimage *f, const struct xvimage *m, int32_t xc, int32_t yc);
+extern int32_t ldilateros_ldilatfast(struct xvimage *f, const uint8_t *mask) ;
+extern int32_t ldilateros_lerosfast(struct xvimage *f, const uint8_t *mask);
+extern int32_t ldilateros_ldilat(struct xvimage *f, const struct xvimage *m, int32_t xc, int32_t yc);
+extern int32_t ldilateros_leros(struct xvimage *f, const struct xvimage *m, int32_t xc, int32_t yc);
 extern int32_t ldilat2(struct xvimage *f, int32_t nptb, int32_t *tab_es_x, int32_t *tab_es_y, int32_t xc, int32_t yc);
 extern int32_t ldilat3(struct xvimage *f, int32_t nptb, int32_t *tab_es_x, int32_t *tab_es_y, int32_t *tab_es_z, int32_t xc, int32_t yc, int32_t zc);
-extern int32_t ldilatlong(struct xvimage *f, struct xvimage *m, int32_t xc, int32_t yc);
+extern int32_t ldilatlong(struct xvimage *f, const struct xvimage *m, int32_t xc, int32_t yc);
 extern int32_t leros2(struct xvimage *f, int32_t nptb, int32_t *tab_es_x, int32_t *tab_es_y, int32_t xc, int32_t yc);
 extern int32_t leros3(struct xvimage *f, int32_t nptb, int32_t *tab_es_x, int32_t *tab_es_y, int32_t *tab_es_z, int32_t xc, int32_t yc, int32_t zc);
-extern int32_t leroslong(struct xvimage *f,struct xvimage *m,int32_t xc,int32_t yc);
+extern int32_t leroslong(struct xvimage *f, const struct xvimage *m,int32_t xc,int32_t yc);
 extern int32_t ldilateros_lasf(struct xvimage *f, int32_t rayonmax, int32_t rayonmin);
 #ifdef __cplusplus
 }
