bcb3e02795f9da56aa988f96cd130e91ca1e052c
Laszlo Marak <ujoimro@gmail.com>
2014-03-30 03:59:38 +0200

    pydevel.cpp marked for removal

--- src/python/pydevel.cpp
+++ src/python/pydevel.cpp
@@ -30,48 +30,9 @@ using boost::python::def;
 
 void pydevel()
 {
-  //CALL_EXPORTED_FUNCTIONS(BOOST_PP_COUNTER);
-
 # ifdef PINK_DEVELOPMENT
 
-  // def( "circle_tangent", &pink::gsl::circle_tangent,
-  //      (boost::python::arg("x coordinates"), boost::python::arg("y coordinates"), boost::python::arg("point of derivation")),
-  //      "This function estimates the derivativ of the function given by points. It "
-  //      " models a circle and returns its tangent at the given point."
-  //   );
 
-  // def( "fit_circle", &pink::py_fit_circle,
-  //      (boost::python::arg("x coordinates"), boost::python::arg("y coordinates"),boost::python::arg("filename")=""),
-  //      "This function fits a circle to the given points. It returns a vector for the following formula:\n"
-  //      "  a*x^2 + a*y^2 + b*x + c*y + d == 0\n"
-  //      "If the argument filename is given, than the function will generate an example Mathematica\n"
-  //      "code with the circle and the points."
-  //      "The algorithm is a modified version of that in:\n"
-  //      "Andrew Fitzgibbon, Maurizio Pilu, Robert B. Fisher, 'Direct Least Square Fitting of Ellipses', IEEE Transactions on Pattern Analysis and Machine Intelligence, vol. 21, no. 5, pp. 476-480, May 1999, doi:10.1109/34.765658\n"
-  //   );
-
-  // def( "circle_equation_to_coordinates", &pink::py_circle_equation_to_coordinates,
-  //      (boost::python::arg("equation")),
-  //      "This function converts the vector of equation coordinates to a vector of geometric coordinates:\n"
-  //      "the input vector is [a,b,c,d], where\n"
-  //      "  a*x^2 + a*y^2 + b*x + c*y + d == 0\n"
-  //      "the output vector is [center_x, center_y, r], where\n"
-  //      "  (x-center_x)^2 + (y-center_y)^2 == r^2 "
-  //   );
-
-  // def(
-  //   "gradient_abs",
-  //   pink::uiGradientAbs,
-  //   (arg("image")),
-  //   doc__gradient_abs__cpp__
-  //   );
-
-  // def(
-  //   "measure",
-  //   pink::lmeasure<pink::float_image>,
-  //   (arg("image")),
-  //   doc__measure__cpp__
-  //   );
 
 # endif /* PINK_DEVELOPMENT */
 
