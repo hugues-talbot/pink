619d153d04c24b57d9e32b7b8442969c394f2b91
Laszlo Marak <ujoimro@gmail.com>
2014-04-23 15:12:33 +0200

    asf operator wrapped in python (not yet finished)

--- src/python/pyasfbin.cpp
+++ src/python/pyasfbin.cpp
@@ -10,111 +10,94 @@
   ujoimro@gmail.com
 */
 
+
+
+#include "pyexport.hpp"
+
 #include "ldist.h"
-#include "pink_python.h"
+#include "pyasfbin.hpp"
+
+using pink::char_image;
 
 
-using namespace boost::python;
-using namespace pink;
+char_image pink::python::asfbin(
+  const char_image & image,
+  const index_t & max_radius,
+  const index_t & min_radius
+  )
+{
+  char_image result;
+  result = image.clone();      
 
-namespace pink {
-  namespace python {
+  int32_t rayonmin=min_radius, rayonmax=max_radius, rayon;
+  int32_t FERMETURE_EN_1 = 0;
 
-    char_image asfbin(
-      const char_image & image,
-      int max_radius,
-      int min_radius = 1
-      )
+  if (rayonmin == 0) 
+  {
+    rayonmin = 1;
+    FERMETURE_EN_1 = 1;
+  }
+    
+  for (rayon = rayonmin; rayon <= rayonmax; rayon++)
+  {
+#ifdef VERBOSE
+    fprintf(stderr, "%s: rayon = %d\n", argv[0], rayon);
+#endif
+
+    if (FERMETURE_EN_1)
     {
-      char_image result;
-      result = image.clone();      
-
-      int32_t rayonmin=min_radius, rayonmax=max_radius, rayon;
-      int32_t FERMETURE_EN_1 = 0;
-
-      if (rayonmin == 0) 
+      /* fermeture par l'element structurant */
+      if (! ldilatball(result, rayon, 0))
       {
-        rayonmin = 1;
-        FERMETURE_EN_1 = 1;
+        pink_error("function ldilatball failed");
       }
-    
-      for (rayon = rayonmin; rayon <= rayonmax; rayon++)
+      if (! lerosball(result, rayon, 0))
       {
-        #ifdef VERBOSE
-        fprintf(stderr, "%s: rayon = %d\n", argv[0], rayon);
-        #endif
-
-        if (FERMETURE_EN_1)
-        {
-          /* fermeture par l'element structurant */
-          if (! ldilatball(result, rayon, 0))
-          {
-            pink_error("function ldilatball failed");
-          }
-          if (! lerosball(result, rayon, 0))
-          {
-            pink_error("function lerosball failed");
+        pink_error("function lerosball failed");
             
-          }
-          /* ouverture par l'element structurant */
-          if (! lerosball(result, rayon, 0))
-          {
-            pink_error("function lerosball failed");
+      }
+      /* ouverture par l'element structurant */
+      if (! lerosball(result, rayon, 0))
+      {
+        pink_error("function lerosball failed");
             
-          }
-          if (! ldilatball(result, rayon, 0))
-          {
-            pink_error("function ldilatball failed");
+      }
+      if (! ldilatball(result, rayon, 0))
+      {
+        pink_error("function ldilatball failed");
             
-          }
-        }
-        else
-        {
-          /* ouverture par l'element structurant */
-          if (! lerosball(result, rayon, 0))
-          {
-            pink_error("function lerosball failed");
+      }
+    }
+    else
+    {
+      /* ouverture par l'element structurant */
+      if (! lerosball(result, rayon, 0))
+      {
+        pink_error("function lerosball failed");
             
-          }
-          if (! ldilatball(result, rayon, 0))
-          {
-            pink_error("function ldilatball failed");
+      }
+      if (! ldilatball(result, rayon, 0))
+      {
+        pink_error("function ldilatball failed");
             
-          }
-          /* fermeture par l'element structurant */
-          if (! ldilatball(result, rayon, 0))
-          {
-            pink_error("function ldilatball failed");
+      }
+      /* fermeture par l'element structurant */
+      if (! ldilatball(result, rayon, 0))
+      {
+        pink_error("function ldilatball failed");
             
-          }
-          if (! lerosball(result, rayon, 0))
-          {
-            pink_error("function lerosball failed");
+      }
+      if (! lerosball(result, rayon, 0))
+      {
+        pink_error("function lerosball failed");
             
-          }
-        }
+      }
+    }
 
-      } /* for (rayon = 1; rayon <= rayonmax; rayon++) */
+  } /* for (rayon = 1; rayon <= rayonmax; rayon++) */
 
-      return result;      
-    } /* asfbin */
-    
-
-
-  } /* namespace python */
-} /* namespace pink */
-
-// UI_EXPORT_ONE_FUNCTION(
-//   asfbin,
-//   pink::python::asfbin,
-//   ( arg("image"), arg("max radius"), arg("min radius")=1),
-//   doc__asfbin__c__
-//   );
-
-
-
-
-
+  return result;      
+} /* asfbin */
 
 
 
