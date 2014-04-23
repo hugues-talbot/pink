045ec34c2e8bd76c8240a6289ac1ff1594318a13
Laszlo Marak <ujoimro@gmail.com>
2014-04-23 15:07:03 +0200

    main python wrapper refactored

--- src/python/pypink.cpp
+++ src/python/pypink.cpp
@@ -18,90 +18,40 @@
 #include "python_doc.h"
 
 /*! \file pypink.c
-  For exporting functions from pink you should make them into the following
-  format:
-
+  You can export functions from Pink to Python in one of the following formats:
+  
   * In C
-  int pink_c_function( xvimage * image, type_1 var_1, type_2 var_2, ..., type_n var_n)
-
+  int32_t pink_c_function( xvimage * image, type_1 var_1, type_2 var_2, ..., type_n var_n)
+  struct xvimage * pink_c_function( type_0 var_0, type_1 var_1, type_2 var_2, ..., type_n var_n)
+  int32_t pink_c_function( const xvimage * image, type_1 var_1, type_2 var_2, ..., type_n var_n, xvimage * result )
+  result_t pink_c_function(type_0 var_0, type_1 var_1, type_2 var_2, ..., type_n var_n) // note result_t is NOT an image
+  
   You may use printf and you may call exit(1) if there's an error.
   type_k can be any type that python recognizes (int, float, ...) and xvimage*
   The return value is 1 on success and 0 otherwise. To export this function
   you should include a function call in pypink.cpp of the form
 
-  def( "function's name in python",
-  &make_function<char_image, type_1, type_2, ..., type_n, &pink_c_function>,
-  (arg("argument 1 name"), arg("argument 2 name"), ..., arg(argument n name) )
-  "documentation of my function"
-  )
-
-  make_function is a template. In the first parameter you specify the image type. If
-  you want all image types to be exported you put 'base_image' as type. Second, you
-  specify the types of the parameters and last you put the pointer to your pink function.
-  Third you may specify the names of the parameters to appear in python's help. Finally
-  you can put a string with the documentation which will appear in the python's help.
+  allocdef( const char * function name, Fn function_pointer, arg_t arguments' description, const char * documentation )
 
   Example:
-  def( "ptisolated",
-  &make_function<char_image, int, &lptisolated>,
+  allocdef( "ptisolated",
+  lptisolated,
   ( arg("image"), arg("connexity") ),
   doc__ptisolated__c__
   );
 
+  You can use the following functions (in the same order as the function declarations above):
 
+  functiondef = calculate the result in the first parameter
+  allocdef    = allocates the result image and returns a pointer
+  resultdef   = calculates the result in the final argument (which is preallocated for the operator before the call )
+  exportdef   = does not create any image result. This means it does not allocate any image, and does not alter any of it's parameters
+                this type of function is used by the exporting functions ( writeimage, etc...)
   * in C++
 
-  You should write your function in format
-
-  char_image cpp_function( const char_image & image, type_1 var_1, type_2 var_2, ..., type_n var_n )
-  here you can put any image type: char_image, int_image, float_image...
-
-  or
-
-  template <class image_type>
-  image_type cpp_function( const image_type & image, type_1 var_1, type_2 var_2, ..., type_n var_n )
-
-  You can call 'cout <<' as well as you can use exceptions. By convention you should never change the
-  input image. Allways make a copy of it before processing. Remember that char_image is a SHALLOW TYPE
-  char_image image2 (image1) will NOT make a copy. On the other hand you can use function(char_image I)
-  as a parameter-type without the copy penalty.
-  To export this function you should call
-
-  UI_DEFINE_ONE_FUNCTION(
-  function_name_in_python,
-  cpp_function,
-  ( arg("parameter 1 name"), ..., arg("parameter n name") ),
-  "documentation of cpp_function"
-  )
-
-  or, if you have a template function:
-
-  UI_DEFINE_FUNCTION(
-  function_name_in_python,
-  cpp_function,
-  ( arg("parameter 1 name"), ..., arg("parameter n name") ),
-  "documentation of cpp_function"
-  )
-
-  Examples:
-  UI_DEFINE_ONE_FUNCTION(
-  float2byte,
-  pink::float2byte,
-  ( arg("source image"), arg("mode")=2 ),
-  doc__float2byte__c__
-  // end of the documenation
-  );
-
-  UI_DEFINE_FUNCTION(
-  convert2float,
-  pink::convert2float,
-  (arg("image")),
-  "converts an image to float type"
-  // end of the documenation
-  );
-
-
+  You should write your function in format WRITE ME!!!!
 */
+
 // declarations
 // void deep_xvimage_object_export();void shallow_xvimage_object_export();
 // void xvimage_object_export();
@@ -112,9 +62,6 @@ void vint_object_export(); void vector_int_object_export();void wshedtopo_export
 // skeleton function
 void skeleton2_export();void zoom_export();
 
-void dilation_export(); void erosion_export(); void geodilat_export();
-void geoeros_export(); void opening_export(); void drawcurve2D_export();
-void uiSqhool_object_export(); void gradient_export();
 // Fast morpho operators
 void fdilaterect_export();
 void fdilatepoly_export();
@@ -130,20 +77,20 @@ void closebin_export();
 
 void read_raw_image_export(); void seuil_export(); void plane3d_export(); void draw_plane_export();
 void project_plane_export(); void border_export(); void identifyline_export(); void surimp_export();
-void generate_rgb_image_export(); void closing_export(); /*void closeball_export();*/ void minmax_export();
+void generate_rgb_image_export(); /*void closeball_export();*/ void minmax_export();
 /*void dilatball_export();*/ void asfbin_export();
 void skelcurv_export(); void distc_export();
 void readimage_export(); void skelsurf_export(); void toposhrink_export(); void htkern_export();
 /*void openball_export();*/ void gradmorph_export(); void mcube_export(); void minima_export();
 void complex_export(); void fft_export(); void mult_export(); void pymaxflow();
 
-// juliette
-int short2long(xvimage *image, xvimage *result);
-int long2short(xvimage *image, xvimage *result);
-int imferode_rect( struct xvimage *input, int SEnx, int SEny, struct xvimage *output);
-int imfdilat_rect( struct xvimage *input, int SEnx, int SEny, struct xvimage *output);
-int imfopen_rect( struct xvimage *input, int SEnx, int SEny, struct xvimage *output);
-int imfclose_rect( struct xvimage *input, int SEnx, int SEny, struct xvimage *output);
+//!!!!!! juliette
+// int short2long(xvimage *image, xvimage *result);
+// int long2short(xvimage *image, xvimage *result);
+// int imferode_rect( struct xvimage *input, int SEnx, int SEny, struct xvimage *output);
+// int imfdilat_rect( struct xvimage *input, int SEnx, int SEny, struct xvimage *output);
+// int imfopen_rect( struct xvimage *input, int SEnx, int SEny, struct xvimage *output);
+// int imfclose_rect( struct xvimage *input, int SEnx, int SEny, struct xvimage *output);
 
 // skeleton
 void pyskeleton();
@@ -160,229 +107,104 @@ void PLiarEnableDebug_export() ; void PLiarDisableDebug_export(); void Pimview_f
 void Pimview_sendcommand_export();
 #endif /* UNIXIO */
 
-
-void pycomplex();
-void pyconversion();
-void pydevel();
-void pynormalization();
-void pyimage_types();
-void pywshed();
-void pypoints();
-void pyliar();
-void pyconnect();
+void pyio();
+void pystat();
+void pyutil();
+void pygeom();
 void pytopo();
+void pystat();
+void pygraph();
+void pyhisto();
+void pywshed();
+void pysignal();
 void pymorpho();
+void pycomplex();
 void pyarithmetic();
-void pymemtest();
-void pysept();
-void numpy_export();
-
-// for wrapping every type with one function
-// template <class image_type>
-// void ujoi_class_export( const char* object_name, const char* object_description );
-
-/// ********************************************************************************************
-/// ********************************************************************************************
-/// ********************************************************************************************
-/// *****                                                                                  *****
-/// *****                          ALTERNATIVE 2011 WRAPPING                               *****
-/// *****                                                                                  *****
-/// ********************************************************************************************
-/// ********************************************************************************************
-/// ********************************************************************************************
-
-
-// UI_WRAP_FUNCTION(
-//   "simplepair",
-//   lsimplepair,
-//   (arg("image"), arg("all")),
-//   doc__simplepair__c__
-//   );
-
-// UI_WRAP_FUNCTION(
-//   "skew",
-//   lskew,
-//   (arg("image"), arg("skew")),
-//   doc__skew__c__
-//   );
-
-// UI_WRAP_FUNCTION(
-//   "squel",
-//   lsquel,
-//   (arg("image"), arg("threshold"), arg("ni threshold")),
-//   doc__squel__c__
-//   );
-
-// UI_WRAP_FUNCTION(
-//   "squelbin",
-//   lsquelbin,
-//   (arg("image"), arg("connexity"), arg("ni threshold")),
-//   doc__squelbin__c__
-//   );
-
-// UI_WRAP_FUNCTION(
-//   "stretch",
-//   lstretch,
-//   (arg("image")),
-//   doc__stretch__c__
-//   );
-
-// UI_WRAP_FUNCTION(
-//   "sym",
-//   lsym,
-//   (arg("image"), arg("mode")),
-//   doc__sym__c__
-//   );
-
-// UI_WRAP_FUNCTION(
-//   "t26pp",
-//   ltopotypes_t26pp,
-//   (arg("image")),
-//   doc__t26pp__c__
-//   );
-
-// UI_WRAP_FUNCTION(
-//   "t4pp",
-//   lt4pp,
-//   (arg("image")),
-//   doc__t4pp__c__
-//   );
-
-// UI_WRAP_FUNCTION(
-//   "t8pp",
-//   lt8pp,
-//   (arg("image")),
-//   doc__t8pp__c__
-//   );
-
-// UI_WRAP_FUNCTION(
-//   "tlf",
-//   ltlf,
-//   (arg("image"), arg("connexmin"), arg("radius")),
-//   doc__tlf__c__
-//   );
-
-// UI_WRAP_FUNCTION(
-//   "translate",
-//   loffset,
-//   (arg("image"), arg("ox"), arg("oy"), arg("oz"), arg("mode") =0),
-//   doc__translate__c__
-//   );
-
-
-using boost::python::arg;
-using boost::python::def;
-
+void pyprimitives();
+void pysegmentation();
 
 BOOST_PYTHON_MODULE(libcpp_pink)
 // the final module will be named pink, but that will also contain
 // some python functions, like readimage, so it will be created as
 // a tree hierarchy
 {
-
 # ifdef UJIMAGE_DEBUG
   std::cerr << "Pink was compiled in debug level " << UJIMAGE_DEBUG << ". You will see various debug messages.\n" <<  std::endl;
 # endif /* UJIMAGE_DEBUG */
 
-  def("greet", greet, "Gently greets the user.");
-
-//   wshedtopo_export(); /* erosball_export();*/  medianfilter_export();
-
-//   // the skeleton function
-//   skeleton2_export();  zoom_export();
-
-//   dilation_export(); erosion_export();  geodilat_export();  geoeros_export();
-//   opening_export();  drawcurve2D_export();
-//   // liar operator functions
-//   fdilaterect_export();
-//   fdilatepoly_export();
-//   feroderect_export();
-//   ferodepoly_export();
-//   fcloserect_export();
-//   fclosepoly_export();
-//   fopenrect_export();
-//   fopenpoly_export();
+  boost::python::def("greet", greet, "Gently greets the user.");
   
-//   openbun_export();
-//   closebin_export();
+//   // submodules
+//   // motivation:
+//   // http://bfroehle.com/2011/07/boost-python-submodules/
+//   // http://isolation-nation.blogspot.hu/2008/09/packages-in-python-extension-modules.html
+//   boost::python::object package = boost::python::scope();
+//   package.attr("__path__") = "libcpp_pink";
 
-//   read_raw_image_export();  seuil_export();  plane3d_export();
-// //  draw_plane_export();
-// //  project_plane_export();
-//   border_export();  identifyline_export();  surimp_export();  generate_rgb_image_export();
-//   closing_export(); /* closeball_export();*/  minmax_export();  /* dilatball_export();*/
-//   asfbin_export();  skelcurv_export();
-//   readimage_export();  distc_export();  skelsurf_export();  toposhrink_export();
-//   htkern_export();  /*openball_export();*/    gradmorph_export(); mcube_export(); minima_export();
-//   complex_export(); fft_export();  mult_export();  numpy_export();
+  
+// //   wshedtopo_export(); /* erosball_export();*/  medianfilter_export();
+
+// //   // the skeleton function
+// //   skeleton2_export();  zoom_export();
+
+// //   dilation_export(); erosion_export();  geodilat_export();  geoeros_export();
+// //   opening_export();  drawcurve2D_export();
+// //   // liar operator functions
+// //   fdilaterect_export();
+// //   fdilatepoly_export();
+// //   feroderect_export();
+// //   ferodepoly_export();
+// //   fcloserect_export();
+// //   fclosepoly_export();
+// //   fopenrect_export();
+// //   fopenpoly_export();
+  
+// //   openbun_export();
+// //   closebin_export();
+
+// //   read_raw_image_export();  seuil_export();  plane3d_export();
+// // //  draw_plane_export();
+// // //  project_plane_export();
+// //   border_export();  identifyline_export();  surimp_export();  generate_rgb_image_export();
+
+//minmax_export();  /* dilatball_export();*/
+// //   asfbin_export();  skelcurv_export();
+// //   readimage_export();  distc_export();  skelsurf_export();  toposhrink_export();
+// //   htkern_export();  /*openball_export();*/    gradmorph_export(); mcube_export(); minima_export();
+// //   complex_export(); fft_export();  mult_export();  numpy_export();
 
 
-  // this part is not yet ready for windows
-# ifdef UNIXIO
-  // is this enough ? HT
-  // this calls the definition in the right place
+//   // this part is not yet ready for windows
+// # ifdef UNIXIO
+//   // is this enough ? HT
+//   // this calls the definition in the right place
 
-  // Pimview_export();
-  // Pimviewlogin_export();
-  // Pimviewputimage_export();
-  // PLiarEnableDebug_export();
-  // Pimview_force_socket_export();
-  // Pimview_sendcommand_export();
+//   // Pimview_export();
+//   // Pimviewlogin_export();
+//   // Pimviewputimage_export();
+//   // PLiarEnableDebug_export();
+//   // Pimview_force_socket_export();
+//   // Pimview_sendcommand_export();
 
-# endif /* UNIXIO */
+// # endif /* UNIXIO */
 
-  // pysept();
-  // pytopo();
-  // pyliar();
-  // pywshed();
-  // pydevel();
-  // pygraph();  
-  // pymorpho();
-  // pypoints();
-  // pymemtest();
-  pyconnect();
-  // pymaxflow();
-  // pycomplex();
-  // pyskeleton();
-  // pyarithmetic();
-  // pyconversion();
-  // pyimage_types();
-  // pynormalization();
+  pyio();
+  pygeom();
+  pyutil();  
+  pytopo();
+  pystat();  
+  pygraph();
+  pyhisto();
+  pywshed();  
+  pysignal();  
+  pymorpho();
+  pycomplex();  
+  pyarithmetic();
+  pyprimitives();
+  pysegmentation();
+  
+  import_array();  // numpy initialization
 
 } /* BOOST_PYTHON_MODULE */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // LuM end of file
