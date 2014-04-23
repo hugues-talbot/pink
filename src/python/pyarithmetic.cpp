203a93c78fb0b65211e49b02c54cad3f8b9522cd
Laszlo Marak <ujoimro@gmail.com>
2014-04-23 15:00:56 +0200

    arithmetic wrappers for python

--- src/python/pyarithmetic.cpp
+++ src/python/pyarithmetic.cpp
@@ -5,175 +5,69 @@
   This software comes in hope that it will be useful but
   without any warranty to the extent permitted by applicable law.
 
+  (C) UjoImro, 2012-2014
+  ProCarPlan s.r.o.
+  ujoimro@gmail.com
   (C) UjoImro, 2011
   Université Paris-Est, Laboratoire d'Informatique Gaspard-Monge, Equipe A3SI, ESIEE Paris, 93162, Noisy le Grand CEDEX
   ujoimro@gmail.com
 */
 // The pink python wrapper core file
 
-#include "pink_python.h"
-#include "ldir.h"
-#include "lccv.h"
-#include "lhtkern.h"
-#include "lconvol3.h"
-#include "legalise.h"
-#include "lcontours.h"
-#include "lhtkern3d.h"
-#include "lhistscal.h"
-#include "ldirections.h"
+#include "pyexport.hpp"
+
+#include "lseuil.h"
+#include "larith.h"
+#include "python_doc.h"
 #include "ldetectcercles.h"
 
-#include <boost/preprocessor/slot/counter.hpp>
+void pyarithmetic()
+{
 
-using boost::python::arg;
-using boost::python::def;
-
-// namespace arithmetic
-// {
-//   UI_WRAP_FUNCTION(
-//     "ccv",
-//     lccv,
-//     (arg("image"), arg("elem")),
-//     doc__ccv__c__
-//     );
-// # include BOOST_PP_UPDATE_COUNTER()
-
-//   UI_WRAP_FUNCTION(
-//     "contours",
-//     lcontours,
-//     (arg("image"), arg("higher threshold"), arg("lower threshold")),
-//     doc__contours__c__
-//     );
-// # include BOOST_PP_UPDATE_COUNTER()
-
-//   UI_WRAP_FUNCTION(
-//     "convol3",
-//     lconvol3,
-//     (arg("image"), arg("mask"), arg("")),
-//     doc__convol3__c__
-//     );
-// # include BOOST_PP_UPDATE_COUNTER()
-
-//   UI_WRAP_FUNCTION(
-//     "detectcercles",
-//     ldetectcercles,
-//     (arg("image"), arg("radius")),
-//     doc__detectcercles__c__
-//     );
-// # include BOOST_PP_UPDATE_COUNTER()
-
-// //NOTE: consider directionalfilter because of the converfloat
-
-//   UI_WRAP_FUNCTION(
-//     "dir",
-//     ldir,
-//     (arg("image"), arg("dir")),
-//     doc__dir__c__
-//     );
-// # include BOOST_PP_UPDATE_COUNTER()
-
-//   UI_WRAP_FUNCTION(
-//     "directions",
-//     ldirections,
-//     (arg("image"), arg("connex"), arg("nbpoints"), arg("normale")),
-//     doc__directions__c__
-//     );
-// # include BOOST_PP_UPDATE_COUNTER()
-
-//   UI_WRAP_FUNCTION(
-//     "dynrecons",
-//     ldynrecons,
-//     (arg("image"), arg("cond image"), arg("connexity")),
-//     doc__dynrecons__c__
-//     );
-// # include BOOST_PP_UPDATE_COUNTER()
-
-//   UI_WRAP_FUNCTION(
-//     "egalise",
-//     legalise,
-//     (arg("image"), arg("n")),
-//     doc__egalise__c__
-//     );
-// # include BOOST_PP_UPDATE_COUNTER()
-
-// // UI_WRAP_FUNCTION(
-// //   "fermetrous3d",
-// //   lfermetrous3dbin,
-// //   (arg("image"), arg("connex"), arg("hole size")),
-// //   doc__fermetrous3d__c__
-// //   );
-// // # include BOOST_PP_UPDATE_COUNTER()
-
-
-// // NOTE: fft up for discussion
-// // NOTE: gettree return value type
-
-// // UI_WRAP_FUNCTION(
-// //   "gradill",
-// //   lgradill,
-// //   ( arg("image"), arg("alpha") ),
-// //   doc__gradill__c__
-// //   );
-// // # include BOOST_PP_UPDATE_COUNTER()
-
-// // UI_WRAP_FUNCTION(
-// //   "gradinf",
-// //   lgradinf,
-// //   (arg("image")),
-// // /*  doc__gradinf__c__*/ "WRITE ME!!!"
-// //   );
-// // # include BOOST_PP_UPDATE_COUNTER()
-
-// // UI_WRAP_FUNCTION(
-// //   "gradinf3d",
-// //   lgradinf3d,
-// //   (arg("image")),
-// //   /* doc__gradinf3d__c__ */ "WRITE ME!!!"
-// //   );
-// // # include BOOST_PP_UPDATE_COUNTER()
-
-// // UI_WRAP_FUNCTION(
-// //   "gradsup",
-// //   lgradsup,
-// //   (arg("image")),
-// //   /*doc__gradsup__c__*/ "WRITE ME!!!"
-// //   );
-// // # include BOOST_PP_UPDATE_COUNTER()
-
-//   UI_WRAP_FUNCTION(
-//     "histscal",
-//     lhistscal3,
-//     (arg("image"), arg("a"), arg("A"), arg("b"), arg("B"), arg("c"), arg("C")),
-//     doc__histscal__c__
-//     );
-// # include BOOST_PP_UPDATE_COUNTER()
-
-//   UI_WRAP_FUNCTION(
-//     "histscal",
-//     lhistscal,
-//     (arg("image"), arg("a"), arg("A"), arg("b"), arg("B")),
-//     doc__histscal__c__
-//     );
-// # include BOOST_PP_UPDATE_COUNTER()
-
-//   UI_WRAP_FUNCTION(
-//   "surfacerestoration",
-//   lsurfacerestoration,
-//   (arg("image"), arg("number of maximal iterations"), arg("connexity")),
-//   doc__surfacerestoration__c__
-//   );
-// # include BOOST_PP_UPDATE_COUNTER()
-
-
+  using pink::type;
+  using pink::allocdef;
+  using pink::resultdef;
+  using pink::functiondef;
   
-// } /* namespace points */
+  using boost::python::arg;
+  using boost::python::def;
+  using boost::python::scope;
+  using boost::python::object;
+  using boost::python::handle;
+  using boost::python::borrowed;
 
-// using namespace arithmetic;
+  // make "from mypackage.arith import <whatever>" work
+  object arith_module(handle<>(borrowed(PyImport_AddModule("libcpp_pink.arith"))));
+  
+  // make "from mypackage import arith" work
+  scope().attr("arith") = arith_module;
+  
+  // set the current scope to the new sub-module
+  scope arith_scope = arith_module;
 
-// void pyarithmetic()
-// {
-//   CALL_EXPORTED_FUNCTIONS(BOOST_PP_COUNTER);
-// }
+  functiondef( "detectcercles", ldetectcercles, (arg("image"), arg("radius")), doc__detectcercles__c__ );
+
+  functiondef( "exp", lexp, (arg("image")), doc__exp__c__ );
+
+  functiondef( "pow", lpow, (arg("image"), arg("p")), doc__pow__c__ );
+  
+  functiondef( "log", llog, (arg("image")), doc__log__c__ );
+    
+  functiondef( "inverse", linverse, (arg("image")), doc__inverse__c__ );
+
+  functiondef( "xor", lxor, (arg("image1"), arg("image2")), doc__xor__c__ );
+
+  functiondef( "seuil", lseuil, (arg("image"), arg("threshold")), doc__seuil__c__ );
+
+  functiondef( "threshold", lseuil, (arg("image"), arg("threshold")), doc__seuil__c__ );
+
+  functiondef( "seuil", lseuil3, (arg("image"), arg("low threshold"), arg("high threshold")), doc__seuil__c__ );
+
+  functiondef( "threshold", lseuil3, (arg("image"), arg("low threshold"), arg("high threshold")), doc__seuil__c__ );
+  
+  import_array();  // numpy initialization
+
+} // pyaritmetic
 
 
 
