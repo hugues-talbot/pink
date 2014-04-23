452ee1d6606244eec2ba19efc1cbdb35b371cd17
Laszlo Marak <ujoimro@gmail.com>
2014-04-23 12:01:27 +0200

    python related includes removed

--- include/ui_fit_circle.hpp
+++ include/ui_fit_circle.hpp
@@ -13,23 +13,16 @@
 #ifndef UI_FIT_CIRCLE_HPP_
 #define UI_FIT_CIRCLE_HPP_
 
-const double epsilon=0.001;
+#include <eigen3/Eigen/Core>
+#include <boost/python/list.hpp>
 
-#ifdef PINK_HAVE_PYTHON
-#  ifdef _WINDOWS
-#    define BOOST_PYTHON_STATIC_LIB
-#  endif /* _WINDOWS */
-#  include <boost/python.hpp>
-#endif /* PINK_HAVE_PYTHON */
+const double epsilon=0.001;
 
 extern "C"
 {
   // this is the lapack generalized eigenvalue calculation function
-  void dggev_(const char*, const char*, const int*,
-	     const double*, const int*, const double*, const int*,
-	     double*, double*, double*,
-	     double*, const int*, double*, const int*,
-	     double*, const int*, int*);
+  void dggev_(const char*, const char*, const int*, const double*, const int*, const double*, const int*,
+	     double*, double*, double*, double*, const int*, double*, const int*, double*, const int*, int*);
 
   // void dggev_(const char* JOBVL, const char* JOBVR, const int* N,
   //             const double* A, const int* LDA, const double* B, const int* LDB,
