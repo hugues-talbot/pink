492ce6620ab59d1a1df3419cf0644287ed6c92f2
Laszlo Marak <ujoimro@gmail.com>
2014-04-23 15:08:28 +0200

    some watershed operators disabled

--- src/python/pywshed.cpp
+++ src/python/pywshed.cpp
@@ -11,108 +11,64 @@
 */
 // The pink python wrapper core file
 
+#include "pyexport.hpp"
 
 #include "llpemeyer.h"
+#include "python_doc.h"
 //#include "llpemeyer.h"
 #include "lwshedtopo.h"
-#include "pink_python.h"
 #include "powerwatsegm.h"
 #include "ldynamique_grimaud.h"
-
-#include <boost/preprocessor/slot/counter.hpp>
 
 using namespace pink;
 using boost::python::arg;
 using boost::python::def;
 
-namespace wshed
+void
+pywshed()
 {
+  using boost::python::scope;
+  using boost::python::handle;  
+  using boost::python::object;
+  using boost::python::borrowed;
   
+  // we create the wshed submodule
+  // motivation:
+  // http://bfroehle.com/2011/07/boost-python-submodules/
+  // http://isolation-nation.blogspot.hu/2008/09/packages-in-python-extension-modules.html
+  
+  // make "from mypackage.IO import <whatever>" work
+  object wshed_module(handle<>(borrowed(PyImport_AddModule("libcpp_pink.wshed"))));
+  
+  // make "from mypackage import IO" work
+  scope().attr("wshed") = wshed_module;
+  
+  // set the current scope to the new sub-module
+  scope wshed_scope = wshed_module;
 
-//   UI_WRAP_FUNCTION(
-//     "watershed",
-//     lwshedtopobin,
-//     ( arg("image"), arg("mark"), arg("connexity") ),
-//     doc__watershed__c__
-//     );
-// # include BOOST_PP_UPDATE_COUNTER()
+  resultdef( VFF_TYP_4_BYTE, "powerwatershedBW", compute_power_watershed_bw, (arg("image_bw"), arg("seeds"), arg("geodesic"), arg("multiple")), "WRITE ME!!!" /*doc__powerwshedval__c__*/ );
 
-// // by HT for Claire
-//   UI_WRAP_FUNCTION(
-//     "watershedMeyer2",
-//     llpemeyer_llpemeyer2,
-//     ( arg("markers"), arg("image"), arg("mask"), arg("connexity") ),
-//     doc__watershedMeyer2__c__
-//     );
-// # include BOOST_PP_UPDATE_COUNTER()
+  resultdef( VFF_TYP_4_BYTE, "powerwatershedCol", compute_power_watershed_col, (arg("image_r"), arg("image_g"), arg("image_b"), arg("seeds"), arg("geodesic"), arg("multiple")), "WRITE ME!!!" /*doc__powerwshedval__c__*/ );
 
-// // by MC
-//   UI_WRAP_FUNCTION(
-//     "watershedMeyer2lab",
-//     llpemeyer2lab,
-//     ( arg("markers"), arg("image"), arg("mask"), arg("connexity") ),
-//     doc__watershedMeyer2lab__c__
-//     );
-// # include BOOST_PP_UPDATE_COUNTER()
+  functiondef( "watershed", lwshedtopobin, ( arg("image"), arg("mark"), arg("connexity") ), doc__watershed__c__ );
 
-// // by MC
-//   UI_WRAP_FUNCTION(
-//     "watershedMeyer2labnomask",
-//     llpemeyer2lab_nomask,
-//     ( arg("markers"), arg("image"), arg("connexity") ),
-//     doc__watershedMeyer2lab__c__
-//     );
-// # include BOOST_PP_UPDATE_COUNTER()
+// by HT for Claire
+  functiondef( "watershedMeyer2", llpemeyer_llpemeyer2, ( arg("markers"), arg("image"), arg("mask"), arg("connexity") ), doc__watershedMeyer2__c__ );
 
-// // by HT for Claire
-//   UI_WRAP_FUNCTION(
-//     "watershedMeyer2NM",
-//     llpemeyer2_nomask,
-//     ( arg("markers"), arg("image"), arg("connexity") ),
-//     doc__watershedMeyer2__c__
-//     );
-// # include BOOST_PP_UPDATE_COUNTER()
+// by MC
+  functiondef( "watershedMeyer2lab", llpemeyer2lab, ( arg("markers"), arg("image"), arg("mask"), arg("connexity") ), doc__watershedMeyer2lab__c__ );
 
-//   UI_WRAP_FUNCTION(
-//     "wshedval",
-//     lwshedval,
-//     (arg("image"), arg("connex")),
-//     doc__wshedval__c__
-//     );
-// # include BOOST_PP_UPDATE_COUNTER()
+// by MC
+  functiondef( "watershedMeyer2labnomask", llpemeyer2lab_nomask, ( arg("markers"), arg("image"), arg("connexity") ), doc__watershedMeyer2lab__c__ );
 
+// by HT for Claire
+  functiondef( "watershedMeyer2NM", llpemeyer2_nomask, ( arg("markers"), arg("image"), arg("connexity") ), doc__watershedMeyer2__c__ );
 
-//     UI_WRAP_RESULT(
-//         int_image,
-//         "powerwatershedCol",
-//         compute_power_watershed_col,
-//         (arg("image_r"), arg("image_g"), arg("image_b"), arg("seeds"), arg("geodesic"), arg("multiple")),
-//         "WRITE ME!!!"
-//         //doc__powerwshedval__c__
-//         );
+  functiondef( "wshedval", lwshedval, (arg("image"), arg("connex")), doc__wshedval__c__ );
 
-// # include BOOST_PP_UPDATE_COUNTER()
-    
-//     UI_WRAP_RESULT(
-//         int_image,
-//         "powerwatershedBW",
-//         compute_power_watershed_bw,
-//         (arg("image_bw"), arg("seeds"), arg("geodesic"), arg("multiple")),
-//         "WRITE ME!!!"
-//         //doc__powerwshedval__c__
-//         );
+  import_array();  // numpy initialization
 
-// # include BOOST_PP_UPDATE_COUNTER()
-
-
-    
-} /* namespace pywshed */
-
-using namespace wshed;
-
-void pywshed()
-{
-//  CALL_EXPORTED_FUNCTIONS(BOOST_PP_COUNTER);
+  return;
 }
 
 
