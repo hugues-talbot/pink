3cc2d46886c81c170443713c3c6c22aa113d43bc
Laszlo Marak <ujoimro@gmail.com>
2014-04-23 11:15:28 +0200

    python wrapper boost::vector to std::tuple conversion

--- src/python/vector_to_tuple.hpp
+++ src/python/vector_to_tuple.hpp
@@ -25,12 +25,16 @@
 namespace pink {
   namespace tmp {
 
+    using std::tuple;
+    using boost::mpl::at_c;
+    using boost::mpl::vector;
+        
     template <class vector_t, class sequence_t>
     struct select;
 
     template <class vector_t, int...ARGS>
     struct select< vector_t, sequence_t<ARGS...> > {
-      typedef typename std::tuple< typename boost::mpl::at_c<vector_t, ARGS>::type... > type;
+      typedef tuple< typename at_c<vector_t, ARGS>::type... > type;
     }; // struct select
   
     template <class vector_t>
@@ -43,7 +47,7 @@ namespace pink {
     struct tuple_to_vector;
 
     template <class...ARGS>
-    struct tuple_to_vector<std::tuple<ARGS...> > {
+    struct tuple_to_vector<tuple<ARGS...> > {
       typedef boost::mpl::vector<ARGS...> type;      
     }; // tuple_to_vector
 
@@ -52,10 +56,42 @@ namespace pink {
     struct push_result_front;
 
     template <class result_type, class...ARGS>
-    struct push_result_front<result_type, std::tuple<ARGS...> > {
+    struct push_result_front<result_type, tuple<ARGS...> > {
       typedef boost::mpl::vector<result_type, ARGS...> type;      
     }; // tuple_to_vector
 
+    template <class V1, class V2>
+    struct concat;
+
+    template <class...V1_ARGS, class...V2_ARGS>
+    struct concat< tuple<V1_ARGS...>, tuple<V2_ARGS...> > {
+      typedef tuple<V1_ARGS..., V2_ARGS...> type;
+    }; // struct concat 
+
+    template <class V1, class V2>
+    struct vconcat {
+      typedef typename tuple_to_vector< typename concat< typename vector_to_tuple<V1>::type, typename vector_to_tuple<V2>::type >::type >::type type;
+    }; // struct vconcat
+        
+    
+    template <class tuple_t>
+    struct remove_first;
+
+    template <class FIRST, class...REST>
+    struct remove_first< tuple<FIRST, REST...> > {
+      typedef tuple<REST...> type;
+    }; // struct remove_first
+        
+    template <class tuple_t>
+    struct remove_last;
+
+    template <class...ARGS>
+    struct remove_last< tuple<ARGS...> > {
+      const static int size = sizeof...(ARGS);
+      typedef typename select< vector<ARGS...>, typename generate_sequence<size-1>::type >::type type;
+    }; // struct remove_last
+          
+    
   } // namespace tmp
 } // namespace pink
 
