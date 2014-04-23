3896d53cd18b8aa4fa18f68c879c25a199f1fd1f
Laszlo Marak <ujoimro@gmail.com>
2014-04-23 15:11:35 +0200

    complex operators' wrappers refactored (not finished)

--- src/python/pycomplex.cpp
+++ src/python/pycomplex.cpp
@@ -5,18 +5,21 @@
   This software comes in hope that it will be useful but
   without any warranty to the extent permitted by applicable law.
 
+  (C) UjoImro, 2014
+  ProCarPlan s.r.o.
+  ujoimro@gmail.com
+  
   (C) UjoImro, 2011
   Université Paris-Est, Laboratoire d'Informatique Gaspard-Monge, Equipe A3SI, ESIEE Paris, 93162, Noisy le Grand CEDEX
   ujoimro@gmail.com
 */
-// The pink python wrapper core file
+
+#include "pyexport.hpp"
 
 #include "lfft.h"
 #include "larith.h"
-#include "pink_python.h"
+#include "python_doc.h"
 #include "ui_any_to_complex.hpp"
-
-#include <boost/preprocessor/slot/counter.hpp>
 
 // ***********************************************************************
 // ***********************************************************************
@@ -26,72 +29,45 @@
 // ***********************************************************************
 // ***********************************************************************
 
-using namespace pink;
-using namespace boost::python;
-
-namespace complex
+void
+pycomplex()
 {
+  using pink::type;
+  using pink::allocdef;
+  using pink::resultdef;
+  using pink::functiondef;
   
-//   UI_WRAP_CPP(
-//     "any2complex",
-//     pink::lany2complex,
-//     (arg("real part"), arg("imaginary part")),
-//     doc__ui_any_to_complex__c__
-//     );
-// # include BOOST_PP_UPDATE_COUNTER()
+  using boost::python::arg;
+  using boost::python::def;
+  using boost::python::scope;
+  using boost::python::object;
+  using boost::python::handle;
+  using boost::python::borrowed;
 
-//   UI_WRAP_FUNCTION(
-//     "fft",
-//     lfft,
-//     (arg("image"), arg("direction")),
-//     doc__fft__c__
-//     );
-// # include BOOST_PP_UPDATE_COUNTER()
+  // make "from mypackage.complex import <whatever>" work
+  object complex_module(handle<>(borrowed(PyImport_AddModule("libcpp_pink.complex"))));
+  
+  // make "from mypackage import complex" work
+  scope().attr("complex") = complex_module;
+  
+  // set the current scope to the new sub-module
+  scope complex_scope = complex_module;
 
-//   UI_WRAP_RESULT(
-//     float_image,
-//     "complex_modulus",
-//     lmodulus,
-//     (arg("image")),
-//     doc__complex_modulus__c__
-//     );
-// # include BOOST_PP_UPDATE_COUNTER()
+  // UI_WRAP_CPP( "any2complex", pink::lany2complex, (arg("real part"), arg("imaginary part")), doc__ui_any_to_complex__c__ );
 
-//   UI_WRAP_RESULT(
-//     float_image,
-//     "modulus",
-//     lmodulus,
-//     (arg("image")),
-//     "WRITE ME!!!"
-//     );
-// #include BOOST_PP_UPDATE_COUNTER()
+  // functiondef( "fft", lfft, (arg("image"), arg("direction")), doc__fft__c__ );
 
-//   UI_WRAP_RESULT(
-//     float_image,
-//     "real",
-//     lreal,
-//     (arg("image")),
-//     "WRITE ME!!!"
-//     );
-// #include BOOST_PP_UPDATE_COUNTER()
+  resultdef( VFF_TYP_FLOAT, "complex_modulus", lmodulus, (arg("image")), doc__complex_modulus__c__ );
 
-//   UI_WRAP_RESULT(
-//     float_image,
-//     "imaginary",
-//     limaginary,
-//     (arg("image")),
-//     "WRITE ME!!!"
-//     );
-// #include BOOST_PP_UPDATE_COUNTER()
+  resultdef( VFF_TYP_FLOAT, "modulus", lmodulus, (arg("image")), "WRITE ME!!!" );
 
+  resultdef( VFF_TYP_FLOAT, "real", lreal, (arg("image")), "WRITE ME!!!" );
 
-} /* namespace complex */
+  resultdef( VFF_TYP_FLOAT, "imaginary", limaginary, (arg("image")), "WRITE ME!!!" );
+   
+  import_array();  // numpy initialization
 
-using namespace complex;
-
-void pycomplex()
-{
-  // CALL_EXPORTED_FUNCTIONS(BOOST_PP_COUNTER);
-}
+  return;  
+} // pycomplex
 
 
