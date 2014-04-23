42a381c93665f13e095fc36dded08eb4a906203a
Laszlo Marak <ujoimro@gmail.com>
2014-04-23 11:18:32 +0200

    constantification of the drawing operators

--- include/ldraw.h
+++ include/ldraw.h
@@ -39,26 +39,27 @@ knowledge of the CeCILL license and that you accept its terms.
 extern "C" {
 #endif
 
-extern int32_t ldrawline(struct xvimage * image1, int32_t x1, int32_t y1, int32_t x2, int32_t y2);
-extern int32_t ldrawlinelist(int32_t *lx, int32_t *ly, int32_t *n, int32_t x1, int32_t y1, int32_t x2, int32_t y2);
-extern int32_t ldrawline3d(struct xvimage * image1, int32_t x1, int32_t y1, int32_t z1, int32_t x2, int32_t y2, int32_t z2);
-extern int32_t ldrawline3dlist(int32_t *lx, int32_t *ly, int32_t *lz, int32_t *npoints, int32_t x1, int32_t y1, int32_t z1, int32_t x2, int32_t y2, int32_t z2);
-extern int32_t ldrawline2(struct xvimage * image1);
-extern void ldrawfilledellipse(struct xvimage * image, double R, double S, double T, double U, double V, double Z);
-extern void ldrawcubic1(struct xvimage * image1, double *x, double *y, int32_t nseg, double sx, double sy);
-extern void ldrawcubic2(struct xvimage * image1, double *x, double *y, int32_t nseg, double tmin, double tmax);
-extern void ldrawcubic2list(int32_t *lx, int32_t *ly, int32_t *npoints, double *x, double *y, int32_t nseg, double tmin, double tmax);
-extern void ldrawcubic3d(struct xvimage * image1, double *x, double *y, double *z, int32_t nseg, double tmin, double tmax);
-extern void ldrawcubic3dlist(int32_t *lx, int32_t *ly, int32_t *lz, int32_t *npoints, double *x, double *y, double *z, int32_t nseg, double tmin, double tmax);
-extern int32_t ldrawball(struct xvimage * image1, double r, double xc, double yc, double zc);
-extern void ldrawdisc(struct xvimage * image1, double r, double xc, double yc);
-extern int32_t ldrawtorus(struct xvimage * image1, double c, double a, double xc, double yc, double zc);
-extern void ldrawtangents2d(struct xvimage *field, double *x, double *y, int32_t nseg, double tmin, double tmax);
-extern void ldrawtangents3d(struct xvimage *field, double *x, double *y, double *z, int32_t nseg, double tmin, double tmax);
-extern void ldrawfield2dlist(int32_t npoints, int32_t *X, int32_t *Y, double *tx, double *ty, struct xvimage *image, double len);
-extern void ldrawfield3dlist(int32_t npoints, int32_t *X, int32_t *Y, int32_t *Z, double *tx, double *ty, double *tz, struct xvimage *image, double len);
-extern struct xvimage *ldrawfield2d(struct xvimage *field, double len);
-extern struct xvimage *ldrawfield3d(struct xvimage *field, double len);
+  extern int32_t ldrawline(struct xvimage * image1, int32_t x1, int32_t y1, int32_t x2, int32_t y2);
+  extern int32_t ldrawlinelist(int32_t *lx, int32_t *ly, int32_t *n, int32_t x1, int32_t y1, int32_t x2, int32_t y2);
+  extern int32_t ldrawline3d(struct xvimage * image1, int32_t x1, int32_t y1, int32_t z1, int32_t x2, int32_t y2, int32_t z2);
+  extern int32_t ldrawline3dlist(int32_t *lx, int32_t *ly, int32_t *lz, int32_t *npoints, int32_t x1, int32_t y1, int32_t z1, int32_t x2, int32_t y2, int32_t z2);
+  extern int32_t ldrawline2(struct xvimage * image1);
+  extern int32_t ldrawfilledellipse(struct xvimage * image, double R, double S, double T, double U, double V, double Z);
+  extern void ldrawcubic1(struct xvimage * image1, double *x, double *y, int32_t nseg, double sx, double sy);
+  extern void ldrawcubic2(struct xvimage * image1, double *x, double *y, int32_t nseg, double tmin, double tmax);
+  extern void ldrawcubic2list(int32_t *lx, int32_t *ly, int32_t *npoints, double *x, double *y, int32_t nseg, double tmin, double tmax);
+  extern void ldrawcubic3d(struct xvimage * image1, double *x, double *y, double *z, int32_t nseg, double tmin, double tmax);
+  extern void ldrawcubic3dlist(int32_t *lx, int32_t *ly, int32_t *lz, int32_t *npoints, double *x, double *y, double *z, int32_t nseg, double tmin, double tmax);
+  extern int32_t ldrawball(struct xvimage * image1, double r, double xc, double yc, double zc);
+  extern void ldrawdisc(struct xvimage * image1, double r, double xc, double yc);
+  extern int32_t ldrawtorus(struct xvimage * image1, double c, double a, double xc, double yc, double zc);
+  extern void ldrawtangents2d(struct xvimage *field, double *x, double *y, int32_t nseg, double tmin, double tmax);
+  extern void ldrawtangents3d(struct xvimage *field, double *x, double *y, double *z, int32_t nseg, double tmin, double tmax);
+  extern void ldrawfield2dlist(int32_t npoints, int32_t *X, int32_t *Y, double *tx, double *ty, struct xvimage *image, double len);
+  extern void ldrawfield3dlist(int32_t npoints, int32_t *X, int32_t *Y, int32_t *Z, double *tx, double *ty, double *tz, struct xvimage *image, double len);
+  extern struct xvimage * ldrawfield ( const struct xvimage * field, double len );  
+  extern struct xvimage *ldrawfield2d( const struct xvimage *field, double len);
+  extern struct xvimage *ldrawfield3d( const struct xvimage *field, double len);
 #ifdef __cplusplus
 }
 #endif
