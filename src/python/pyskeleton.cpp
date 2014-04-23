a9d21b56b14d89e7d2a6327aa004075d09b0b503
Laszlo Marak <ujoimro@gmail.com>
2014-04-23 15:07:43 +0200

    skeleton operator refactored

--- src/python/pyskeleton.cpp
+++ src/python/pyskeleton.cpp
@@ -30,153 +30,90 @@
 namespace pink {
   namespace python {
 
-//prototype
-    char_image skeleton_ultimate(
-      const char_image image, 
-      const int_image  prio,
-      index_t          priocode,
-      index_t          connex, 
-      const char_image inhibit,
-      index_t          inhibitcode
-      );
+    using pink::pick;    
+    using boost::python::object;
+    using boost::python::extract;
+        
+    object
+    skeleton(
+      const object  image,
+      const object  cprio,
+      const int32_t connex,
+      const object  inhibit ) {
 
+      cxvimage prioimage;      
+      int32_t  priocode = -1;
+      cxvimage inhibimage;
+      int32_t  inhibvalue = -1;
 
+      cxvimage result = cxvimage(image).clone();
 
-/* nc == non_const */
-
-    char_image
-    skeleton_im_int_int_void ( const char_image & image, index_t priocode, index_t connex ) {
-      return skeleton_ultimate( image, int_image(), priocode, connex, char_image(), -1 );
-    } /* skeleton_im_int_int_void */
-
-
-    char_image
-    skeleton_im_im_int_void ( const char_image & image, const int_image & prio, index_t connex ) {
-      return skeleton_ultimate( image, prio, -1, connex, char_image(), -1 );
-    } /* skeleton_im_im_int_void */
-
-    char_image
-    skeleton_im_int_int_im( const char_image & image, index_t prio, index_t connex, const char_image & inhibit ) {
-      return skeleton_ultimate( image, int_image(), prio, connex, inhibit, -1 );
-    };
-
-    char_image
-    skeleton_im_im_int_im( const char_image & image, const int_image & prio, index_t connex, const char_image & inhibit ) {
-      return skeleton_ultimate( image, prio, -1, connex, inhibit, -1 );
-    };
-
-    char_image
-    skeleton_im_int_int_int( const char_image & image, index_t prio, index_t connex, index_t inhibit_value ) {
-      return skeleton_ultimate( image, int_image(), prio, connex, char_image(), inhibit_value );
-    };
-
-    char_image
-    skeleton_im_im_int_int( const char_image & image, const int_image & prio, index_t connex, index_t inhibit_value ) {
-      return skeleton_ultimate( image, prio, -1, connex, char_image(), inhibit_value );
-    };
-
-  
-    char_image
-    skeleton_ultimate(
-      const char_image image, 
-      const int_image  prio,
-      const index_t    priocode,
-      const index_t    connex, 
-      const char_image inhibit,
-      const index_t    inhibitvalue
-      )
-    {
-
-      char_image result = image.clone();
-      int_image m_prio;
-      
-      index_t dim = image.size().size();
-      if ((dim<2) || (dim>3)) pink_error("only 2D and 3D images are supported.");
-            
-      if (!prio.isnull())
-      {
-        m_prio = prio;        
-      }
+      if (PyArray_Check(cprio.ptr())) // prio is an image
+        prioimage = cxvimage(cprio);            
       else
-      { 
-        // the distance map has to be calculated
-        index_t N;
+        priocode = extract<index_t>(cprio);
+
+      if (PyArray_Check(inhibit.ptr()))
+        inhibimage = cxvimage(inhibit);
+      else
+        inhibvalue = extract<index_t>(inhibit);
+
+      // if the priority is some kind of distance
+      if (prioimage.isnull()) {
+        int32_t i, N;
         uint8_t *F;
-        m_prio.reset(result.size());
-    
-        N = pink::prod(result.size());
-        F = result.get();
-
-        // we inverse the result
-        for (index_t i = 0; i < N; i++) 
+        prioimage = cxvimage( VFF_TYP_4_BYTE, result.size() );
+        
+        N = result.rows() * result.cols() * result.depth();
+        F = result.pdata<uint8_t>();        
+        for (i = 0; i < N; i++) // inverse l'image
           if (F[i]) F[i] = 0; else F[i] = NDG_MAX;
-    
-        // calculating the appropriate distance map
-        switch ( priocode )
+
+        bool is2d = (result.depth()==1);        
+        
+        if (priocode == 0)
+          pick( "ldisteuc(3d)", is2d, ldisteuc, ldisteuc3d, result, prioimage );
+        else if (priocode == 1)
+          pick( "ldistquad(3d)", is2d, ldistquad, ldistquad3d, result, prioimage );        
+        else if (priocode == 2) {
+          if (!lchamfrein(result, prioimage)) pink_error("lchamfrein failed");
+        }
+        else if (priocode == 3)
         {
-      
-        case 0:
-          if ( result.depth() == 1 )
-          { if (! ldisteuc(result, m_prio ) ) pink_error("ldisteuc failed") }
-          else
-          { if (! ldisteuc3d( result, m_prio ) ) pink_error("ldisteuc3d failed"); }
-          break;
-    
-        case 1:      
-          if ( result.depth() == 1 )
-          { if (! ldistquad( result, m_prio ) ) pink_error("ldistquad failed") }
-          else
-          { if (! ldistquad3d(result, m_prio ) ) pink_error("ldistquad3d failed") }
-          break;
+          if (! lsedt_meijster(result, prioimage)) pink_error("lsedt_meijster failed");          
+        }
+        else {
+          if (! ldist(result, priocode, prioimage)) pink_error("ldist failed");          
+        }
 
-        case 2:
-          if (! lchamfrein( result, m_prio ) ) pink_error("lchamfrein failed");
-          break;
-
-        case 3:
-          if (! lsedt_meijster( result, m_prio ) ) pink_error("lsedt_meijster failed");
-          break;
-
-        default:
-          if (! ldist( result, priocode, m_prio ) ) pink_error("ldist failed");
-          break;
-
-        } /* switch (priocode) */
-
-
-        // re-inversing the result
-        for (index_t i = 0; i < N; i++) 
+        for (i = 0; i < N; i++) // re-inverse the image
           if (F[i]) F[i] = 0; else F[i] = NDG_MAX;
+      }
 
-      } /* not if (prio_image) */
-    
       if (result.depth() == 1)
       {
-        if (inhibit.isnull())
-        { if (! lskelubp2( result, m_prio, connex, inhibit ) ) pink_error("lskelubp2 failed"); }
-        else  /* not (inhibit) */
-        {
-          assert(inhibitvalue != -1);
-          if (! lskelubp( result, m_prio, connex, inhibitvalue ) ) pink_error("lskelubp failed");
+        if (inhibimage.isnull() || (inhibvalue == -1)) {
+          if (! lskelubp2(result, prioimage, connex, inhibimage)) pink_error("lskelubp2 failed");
         }
-      } 
-      else /* not  (depth(image) == 1) */ 
+        else {
+          if (! lskelubp(result, prioimage, connex, inhibvalue)) pink_error("lskelubp failed");
+        }
+      }
+      else
       {
-        if (inhibit.isnull())
-        { if (! lskelubp3d2( result, m_prio, connex, inhibit ) ) pink_error("lskelubp3d2 failed"); }
-        else /* not (inhibit) */
+        if (inhibimage.isnull() || (inhibvalue == -1))
         {
-          assert(inhibitvalue != -1);          
-          if (! lskelubp3d( result, m_prio, connex, inhibitvalue ) ) pink_error("lskelubp3d failed");
+          if (! lskelubp3d2(result, prioimage, connex, inhibimage)) pink_error("lskelubp3d2 failed");
         }
-      } /* not (depth(image) == 1) */
+        else
+        {
+          if (! lskelubp3d(result, prioimage, connex, inhibvalue)) pink_error("lskelubp3d failed");
+        }
+      }
 
-
-      return result;
-    };
-
-
-
+      return result.steel();      
+    } // skeleton
+    
 
   } /* namespace python */
 } /* namespace pink */
@@ -187,46 +124,24 @@ namespace pink {
 ***********************************************************************************************
 ***********************************************************************************************
 
-   exported functions
+exported functions
 
 ***********************************************************************************************
 ***********************************************************************************************
 ***********************************************************************************************
- */
+*/
 
-void skeleton_export() 
+void
+skeleton_export() 
 {
-  // def( "skeleton_im_int_int_void", &pink::python::skeleton_im_int_int_void,
-  //      args("image", "priority value", "connexity"),
-  //      "the help is in 'help(pink.skeleton)'"
-  //   );
+  using boost::python::arg;
+  using boost::python::def;
+  using boost::python::object;
+    
+  def( "skeleton", pink::python::skeleton, (arg("image"), arg("prio"), arg("connex"), arg("inhibit") = object(-1) ), doc__skeleton__c__ );
 
-  // def( "skeleton_im_im_int_void", &pink::python::skeleton_im_im_int_void,
-  //      args("image", "priority image", "connexity"),
-  //      "the help is in 'help(pink.skeleton)'"
-  //   );
-
-  // def( "skeleton_im_int_int_im", &pink::python::skeleton_im_int_int_im,
-  //      args("image", "priority value", "connexity", "inhibited pixels"),
-  //      "the help is in 'help(pink.skeleton)'"
-  //   );
-
-  // def( "skeleton_im_im_int_im", &pink::python::skeleton_im_im_int_im,
-  //      args("image", "priority image", "connexity", "inhibited pixels"),
-  //      "the help is in 'help(pink.skeleton)'"
-  //   );
-
-  // def( "skeleton_im_int_int_int", &pink::python::skeleton_im_int_int_int,
-  //      args("image", "priority value", "connexity", "inhibited value"),
-  //      "the help is in 'help(pink.skeleton)'"
-  //   );
-
-  // def( "skeleton_im_im_int_int", &pink::python::skeleton_im_im_int_int,
-  //      args("image", "priority_image", "connexity", "inhibited value"),
-  //      "the help is in 'help(pink.skeleton)'"
-  //   );  
-
- 
+  import_array();  // numpy initialization
+  return;  
 } /* py_skeleton_export */
 
 
