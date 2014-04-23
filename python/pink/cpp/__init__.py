18177f06d8c5b31ce3956a319f776074d3d6a4b8
Laszlo Marak <ujoimro@gmail.com>
2014-04-23 12:10:21 +0200

    imports function into the global namespace

--- python/pink/cpp/__init__.py
+++ python/pink/cpp/__init__.py
@@ -5,6 +5,9 @@
 
 # This software comes in hope that it will be useful but 
 # without any warranty to the extent permitted by applicable law.
+
+# (C) UjoImro <ujoimro@gmail.com>, 2014
+# ProCarPlan s.r.o.
   
 # (C) UjoImro, 2010
 # Université Paris-Est, Laboratoire d'Informatique Gaspard-Monge, Equipe A3SI, ESIEE Paris, 93162, Noisy le Grand CEDEX
@@ -12,3 +15,16 @@
 # 
 
 from libcpp_pink import *
+from libcpp_pink.IO import *
+from libcpp_pink.complex import *
+from libcpp_pink.histo import *
+from libcpp_pink.morpho import *
+from libcpp_pink.segmentation import *
+from libcpp_pink.topo import *
+from libcpp_pink.wshed import *
+from libcpp_pink.arith import *
+from libcpp_pink.geom import *
+from libcpp_pink.primitives import *
+from libcpp_pink.signal import *
+from libcpp_pink.stat import *
+from libcpp_pink.util import *
